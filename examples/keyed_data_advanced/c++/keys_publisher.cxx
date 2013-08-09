dc4fdb65b09cae887bf2beb5dfa31f5e8d885f3b
jmlvega <jmlvega+github@gmail.com>
2013-08-09 23:10:27 +0200

    Removed non ASCII character from files.

--- examples/keyed_data_advanced/c++/keys_publisher.cxx
+++ examples/keyed_data_advanced/c++/keys_publisher.cxx
@@ -241,7 +241,7 @@ extern "C" int publisher_main(int domainId, int sample_count)
     * which data-instance is being modified.
     * However, for performance and semantic reasons, it is better
     * for your application to declare all the data-instances it intends to
-    * modify—prior to actually writing any samples. This is known as registration.
+    * modify prior to actually writing any samples. This is known as registration.
     */
 
     /* In order to register the instances, we must set their associated keys first */
