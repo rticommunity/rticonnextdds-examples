bfe594662e6f2a2ecedc4f80a54dacedab7e01ab
Benito Palacios Sánchez <benito@rti.com>
2015-03-18 18:31:08 +0100

    Changed header and indentation to spaces

--- examples/hello_simple/C++03/Hello_simple_publisher.cxx
+++ examples/hello_simple/C++03/Hello_simple_publisher.cxx
@@ -1,11 +1,13 @@
-/*****************************************************************************/
-/*         (c) Copyright, Real-Time Innovations, All rights reserved.        */
-/*                                                                           */
-/*         Permission to modify and use for internal purposes granted.       */
-/* This software is provided "as is", without warranty, express or implied.  */
-/*                                                                           */
-/*****************************************************************************/
-
+/*******************************************************************************
+ (c) 2005-2014 Copyright, Real-Time Innovations, Inc.  All rights reserved.
+ RTI grants Licensee a license to use, modify, compile, and create derivative
+ works of the Software.  Licensee has the right to distribute object form only
+ for use with RTI products.  The Software is provided "as is", with no warranty
+ of any type, including any warranty for fitness for any purpose. RTI is under
+ no obligation to maintain or support the Software.  RTI shall not be liable for
+ any incidental or consequential damages arising out of the use or inability to
+ use the software.
+ ******************************************************************************/
 #include <iostream>
 #include <dds/pub/ddspub.hpp>
 
@@ -14,56 +16,56 @@ using namespace dds::topic;
 using namespace dds::pub;
 using namespace dds::core;
 
-// By default StringTopicType can manage strings up to 1k
+// By default StringTopicType can manage strings up to 1K
 const int MAX_STRING_SIZE = 1024;
 
 // Domain id
 const int DOMAIN_ID = 0;
 
 int main() {
-	// --- Set Up --------------------------------------------------------- //
+    // --- Set Up --------------------------------------------------------- //
 
-	// To turn on additional logging, include <rti/config/Logger.hpp> and
-	// uncomment the following line:
-	// rti::config::Logger::instance().verbosity(rti::config::Verbosity::STATUS_ALL);
+    // To turn on additional logging, include <rti/config/Logger.hpp> and
+    // uncomment the following line:
+    // rti::config::Logger::instance().verbosity(rti::config::Verbosity::STATUS_ALL);
 
-	// Create a DomainParticipant with the default QoS.
-	DomainParticipant participant (DOMAIN_ID);
+    // Create a DomainParticipant with the default QoS.
+    DomainParticipant participant (DOMAIN_ID);
 
-	// Create a Topic and automatically register the type.
-	Topic<StringTopicType> topic (participant, "HelloSimple");
+    // Create a Topic and automatically register the type.
+    Topic<StringTopicType> topic (participant, "HelloSimple");
 
-	// Create a DataWriter with default QoS.
-	// The publisher is created in-line.
-	DataWriter<StringTopicType> writer (Publisher(participant), topic);
+    // Create a DataWriter with default QoS.
+    // The publisher is created in-line.
+    DataWriter<StringTopicType> writer (Publisher(participant), topic);
 
-	// --- Write Data ----------------------------------------------------- //
+    // --- Write Data ----------------------------------------------------- //
 
-	std::cout << "Ready to write data." << std::endl;
-	std::cout << "When the subscriber is ready, you can start writing." << std::endl;
-	std::cout << "Press CTRL+C to terminate or enter an empty line to do a clean shutdown."
-		      << std::endl
-		      << std::endl;
+    std::cout << "Ready to write data." << std::endl;
+    std::cout << "When the subscriber is ready, you can start writing." << std::endl;
+    std::cout << "Press CTRL+C to terminate or enter an empty line to do a clean shutdown."
+              << std::endl
+              << std::endl;
 
-	// RTI provides APIs for detecting when data readers and data writers
-	// join the network. You can also configure data durability so that data
-	// readers can receive data that were written before they started.
-	// However, for the sake of keeping this example as simple as possible,
-	// it asks you to wait for both sides to start before continuing.
-	char sample[MAX_STRING_SIZE];
-	while (true) {
-		std::cout << "Please type a message> ";
-		if (!cin.getline(sample, MAX_STRING_SIZE-1)) {
-			break;
-		}
+    // RTI provides APIs for detecting when data readers and data writers
+    // join the network. You can also configure data durability so that data
+    // readers can receive data that were written before they started.
+    // However, for the sake of keeping this example as simple as possible,
+    // it asks you to wait for both sides to start before continuing.
+    char sample[MAX_STRING_SIZE];
+    while (true) {
+        std::cout << "Please type a message> ";
+        if (!cin.getline(sample, MAX_STRING_SIZE-1)) {
+            break;
+        }
 
-		writer.write(sample);
+        writer.write(sample);
 
-		if (strlen(sample) == 0) {
-			break;
-		}
-	}
+        if (strlen(sample) == 0) {
+            break;
+        }
+    }
 
-	std::cout << "Shutting down" << std::endl;
-	return 0;
+    std::cout << "Shutting down" << std::endl;
+    return 0;
 }
--- examples/hello_simple/C++03/Hello_simple_subscriber.cxx
+++ examples/hello_simple/C++03/Hello_simple_subscriber.cxx
@@ -1,11 +1,13 @@
-/*****************************************************************************/
-/*         (c) Copyright, Real-Time Innovations, All rights reserved.        */
-/*                                                                           */
-/*         Permission to modify and use for internal purposes granted.       */
-/* This software is provided "as is", without warranty, express or implied.  */
-/*                                                                           */
-/*****************************************************************************/
-
+/*******************************************************************************
+ (c) 2005-2014 Copyright, Real-Time Innovations, Inc.  All rights reserved.
+ RTI grants Licensee a license to use, modify, compile, and create derivative
+ works of the Software.  Licensee has the right to distribute object form only
+ for use with RTI products.  The Software is provided "as is", with no warranty
+ of any type, including any warranty for fitness for any purpose. RTI is under
+ no obligation to maintain or support the Software.  RTI shall not be liable for
+ any incidental or consequential damages arising out of the use or inability to
+ use the software.
+ ******************************************************************************/
 #include <algorithm>
 #include <iostream>
 #include <dds/sub/ddssub.hpp>
@@ -22,66 +24,66 @@ const int DOMAIN_ID = 0;
 // The listener of events and data from the middleware
 class HelloReaderListener : public NoOpDataReaderListener<StringTopicType> {
 public:
-	HelloReaderListener() : must_shutdown(false) { }
+    HelloReaderListener() : must_shutdown(false) { }
 
-	bool get_must_shutdown() {
-		return must_shutdown;
-	}
+    bool get_must_shutdown() {
+        return must_shutdown;
+    }
 
-	void on_data_available(DataReader<StringTopicType>& reader) {
-		// Take all samples
-		LoanedSamples<StringTopicType> samples = reader.take();
+    void on_data_available(DataReader<StringTopicType>& reader) {
+        // Take all samples
+        LoanedSamples<StringTopicType> samples = reader.take();
 
-		for (LoanedSamples<StringTopicType>::iterator sample_it = samples.begin();
-			sample_it != samples.end(); sample_it++) {
+        for (LoanedSamples<StringTopicType>::iterator sample_it = samples.begin();
+            sample_it != samples.end(); sample_it++) {
 
-			// Valid (this isn't just a lifecycle sample): print it
-			if (sample_it->info().valid()) {
-				const dds::core::string& sample = sample_it->data();
-				cout << sample << endl;
+            // Valid (this isn't just a lifecycle sample): print it
+            if (sample_it->info().valid()) {
+                const dds::core::string& sample = sample_it->data();
+                cout << sample << endl;
 
-				// Empty sample to quit
-				if (sample.size() == 0)
-					must_shutdown = true;
-			}
-		}
-	}
+                // Empty sample to quit
+                if (sample.size() == 0)
+                    must_shutdown = true;
+            }
+        }
+    }
 
 private:
-	bool must_shutdown;
+    bool must_shutdown;
 };
 
 int main() {
-	// --- Set Up --------------------------------------------------------- //
+    // --- Set Up --------------------------------------------------------- //
 
-	// To turn on additional logging, include <rti/config/Logger.hpp> and
+    // To turn on additional logging, include <rti/config/Logger.hpp> and
     // uncomment the following line:
     // rti::config::Logger::instance().verbosity(rti::config::Verbosity::STATUS_ALL);
 
-	// Create a DomainParticipant with default QoS.
-	DomainParticipant participant (DOMAIN_ID);
+    // Create a DomainParticipant with default QoS.
+    DomainParticipant participant (DOMAIN_ID);
 
-	// Create a Topic and automatically register the type.
-	Topic<StringTopicType> topic (participant, "HelloSimple");
+    // Create a Topic and automatically register the type.
+    Topic<StringTopicType> topic (participant, "HelloSimple");
 
-	// Create a DataReader with default QoS.
-	// The subscriber is created in-line.
-	DataReader<StringTopicType> reader (Subscriber(participant), topic);
+    // Create a DataReader with default QoS.
+    // The subscriber is created in-line.
+    DataReader<StringTopicType> reader (Subscriber(participant), topic);
 
-	// Set the listener to the subscriber.
-	HelloReaderListener listener;
-	reader.listener(&listener, dds::core::status::StatusMask::data_available());
+    // Set the listener to the subscriber.
+    HelloReaderListener listener;
+    reader.listener(&listener, dds::core::status::StatusMask::data_available());
 
-	// --- Sleep During Asynchronous Reception ---------------------------- //
+    // --- Sleep During Asynchronous Reception ---------------------------- //
 
     // This thread sleeps forever. When a sample is received, RTI Data
     // Distribution Service will call the on_data_available_callback function.
-	std::cout << "Ready to read data." << std::endl;
-	std::cout << "Press CTRL+C to terminate." << std::endl;
-	while (!listener.get_must_shutdown()) {
-		rti::util::sleep(Duration(4));
-	}
+    std::cout << "Ready to read data." << std::endl;
+    std::cout << "Press CTRL+C to terminate." << std::endl;
+    while (!listener.get_must_shutdown()) {
+        rti::util::sleep(Duration(4));
+    }
 
-	std::cout << "Shutting down" << std::endl;
-	return 0;
+    std::cout << "Shutting down" << std::endl;
+    return 0;
 }
