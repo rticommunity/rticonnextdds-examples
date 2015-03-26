c3aae6610ae54522fc4fa9b62acf6c0b311c4213
Benito Palacios Sánchez <benito@rti.com>
2015-03-26 19:41:42 +0100

    Implemented publisher

---
+++ examples/content_filtered_topic/c++03/cft_publisher.cxx
@@ -0,0 +1,100 @@
+/*******************************************************************************
+ (c) 2005-2014 Copyright, Real-Time Innovations, Inc.  All rights reserved.
+ RTI grants Licensee a license to use, modify, compile, and create derivative
+ works of the Software.  Licensee has the right to distribute object form only
+ for use with RTI products.  The Software is provided "as is", with no warranty
+ of any type, including any warranty for fitness for any purpose. RTI is under
+ no obligation to maintain or support the Software.  RTI shall not be liable for
+ any incidental or consequential damages arising out of the use or inability to
+ use the software.
+ ******************************************************************************/
+#include <string>
+#include <dds/pub/ddspub.hpp>
+#include "cft.hpp"
+
+using namespace dds::core;
+using namespace dds::core::policy;
+using namespace dds::domain;
+using namespace dds::pub;
+using namespace dds::pub::qos;
+using namespace dds::topic;
+
+int publisher_main(int domainId, int sample_count)
+{
+    // To customize any of the entities QoS, use
+    // the configuration file USER_QOS_PROFILES.xml
+
+    DomainParticipant participant (domainId);
+
+    Topic<cft> topic (participant, "Example cft");
+
+    DataWriter<cft> writer (Publisher(participant), topic);
+
+    // If you want to set the reliability and history QoS settings
+    // programmatically rather than using the XML, you will need to add
+    // the following lines to your code and comment out the create_datawriter
+    // call above.
+
+    /*
+    DataWriterQos datawriter_qos;
+    datawriter_qos << Reliability::Reliable();
+    datawriter_qos << Durability::TransientLocal();
+    datawriter_qos << History::KeepLast(20);
+    DataWriter<cft> writer (Publisher(participant), topic, datawriter_qos);
+    */
+
+    // Create data sample for writing
+    cft instance;
+
+    // For a data type that has a key, if the same instance is going to be
+    // written multiple times, initialize the key here
+    // and register the keyed instance prior to writing.
+    
+    InstanceHandle instance_handle = InstanceHandle::nil();
+    //instance_handle = writer.register_instance(instance);
+
+    // Main loop
+    for (int count = 0; (sample_count == 0) || (count < sample_count); ++count) {
+        std::cout << "Writing cft, count " << count << std::endl;
+
+        // Modify the data to be sent here
+
+        // Our purpose is to increment x every time we send a sample and to
+        // reset the x counter to 0 every time we send 10 samples (x=0,1,..,9).
+        // Using the value of count, we can get set x to the appropriate value
+        // applying % 10 operation to it.
+        instance.count(count);
+        instance.x(count % 10);
+
+        std::cout << "Writing cft, count " << instance.count() << "\t"
+                  << "x=" << instance.x() << std::endl;
+
+        writer->write(instance, instance_handle);
+
+        // Send a new sample every second
+        rti::util::sleep(Duration(1));
+    }
+
+    writer.unregister_instance(instance_handle);
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int domainId = 0;
+    int sample_count = 0; /* infinite loop */
+
+    if (argc >= 2) {
+        domainId = atoi(argv[1]);
+    }
+
+    if (argc >= 3) {
+        sample_count = atoi(argv[2]);
+    }
+
+    // To turn on additional logging, include <rti/config/Logger.hpp> and
+    // uncomment the following line:
+    // rti::config::Logger::instance().verbosity(rti::config::Verbosity::STATUS_ALL);
+
+    return publisher_main(domainId, sample_count);
+}
