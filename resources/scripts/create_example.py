e1a118d0020796fb3856b2728ef58db51ce5e5c8
Benito Palacios Sánchez <benito@rti.com>
2015-08-05 20:58:48 +0200

    CreateExample script copy files and call rtiddsgen

--- resources/scripts/create_example.py
+++ resources/scripts/create_example.py
@@ -13,6 +13,39 @@
 from jinja2 import Template  # It is not compatible with Python3 yet.
 from argparse import ArgumentParser
 from os import path, makedirs, pardir
+from subprocess import check_call, CalledProcessError
+from re import match, compile
+from shutil import copy2
+
+
+def copy_files(info):
+    # Get IDL file
+    regex = compile('.*\.idl')
+    info["idlfile"] = [file for file in info["files"] if match(regex, file)]
+    info["idlfile"] = info["idlfile"][0] if len(info["idlfile"]) > 0 else None
+
+    # Try to get IDL name if no "--idlname" has been passed
+    if info["idlname"] is None:
+        if info["idlfile"] is not None:
+            info["idlname"] = path.splitext(info["idlfile"])[0]
+        else:
+            info["idlname"] = name
+
+    # Copy files
+    for f in info["files"]:
+        copy2(f, info["expath"])
+
+    # Check if IDL file exist and otherwise generate a Foo structure
+    # TODO
+
+
+def call_rtiddsgen(info):
+    try:
+        # TODO: Check that rtiddsgen is on path or get NDDSHOME env var
+        check_call(["rtiddsgen", "-language", info["language"],
+                   "-example", info["arch"], info["idlfile"]])
+    except CalledProcessError:
+        print("Error calling rtiddsgen")
 
 
 def create_template(info):
@@ -41,11 +74,12 @@ if __name__ == "__main__":
                         'of the folder that contains it.')
     parser.add_argument('fullname', help='Example long name.')
     parser.add_argument('language', help='Programming language.')
-    parser.add_argument('files', help='Extra files to copy like IDL. ' +
-                        'A Foo IDL file will be created if it is not passed',
-                        nargs='*')
+    parser.add_argument('arch', help='Architecture to compile example.')
     parser.add_argument('--idlname', help='The name of the IDL file. ' +
                         'If not specified, "name" will be used.')
+    parser.add_argument('files', help='Extra files to copy like IDL. ' +
+                        'A Foo IDL file will be created if it is not passed',
+                        nargs='+')
     args = parser.parse_args()
 
     # Create info dictionary with the argument info-
@@ -53,7 +87,9 @@ if __name__ == "__main__":
     info["name"] = args.name
     info["fullname"] = args.fullname
     info["language"] = args.language
-    info["idlname"] = args.name if args.idlname == "" else args.idlname
+    info["files"] = args.files
+    info["idlname"] = args.idlname
+    info["arch"] = args.arch    # Used for rtiddsgen
     info["archwindows"] = "x64Win64VS2013"   # It works for all languages.
     info["archlinux"] = "x64Linux3gcc4.8.2"  # It works for all languages.
     info["scriptpath"] = path.dirname(path.realpath(__file__))
@@ -66,10 +102,10 @@ if __name__ == "__main__":
         makedirs(info["expath"])
 
     # Copy files
-    # TODO:
+    copy_files(info)
 
     # Create README templates
     create_template(info)
 
     # Call rtiddsgen to generate files
-    # TODO:
+    call_rtiddsgen(info)
