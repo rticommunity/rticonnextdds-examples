a8aa547f62ea56a243970df05a9def59078c4ce8
Benito Palacios Sánchez <benito@rti.com>
2015-08-14 17:19:05 +0200

    DDSeq: Implement reading

--- examples/dynamic_data_sequences/c++03/dynamic_data_sequences.cxx
+++ examples/dynamic_data_sequences/c++03/dynamic_data_sequences.cxx
@@ -67,17 +67,19 @@ void write_data(DynamicData& sample)
         // See the dynamic_data_nested_structs for further details about the
         // differences between these two APIs.
 
+        // Bind/Loan:
+        // The destructor will unloan the member.
         LoanedDynamicData loaned_data = sequence_member.loan_value(i + 1);
         loaned_data.get().value("a_member", i);
         loaned_data.return_loan();
 
-        // Get/Set:
+        // Set:
         // Create a simple_struct for this sequence element.
         DynamicData simple_struct_element(simple_struct_typecode);
         simple_struct_element.value("a_member", i);
 
         // Add to the sequence.
-        std::cout << "Writing sequence element #" << (i + 1) << " : "
+        std::cout << "Writing sequence element #" << (i + 1) << " :"
                   << std::endl << simple_struct_element;
         sequence_member.value(i + 1, simple_struct_element);
     }
@@ -87,7 +89,31 @@ void write_data(DynamicData& sample)
 
 void read_data(const DynamicData& sample)
 {
+    // Get the sequence member
+    DynamicData sequence_member = sample.value<DynamicData>("sequence_member");
 
+    // Get the total amount of elements contained in the sequence by accessing
+    // the dynamic data info.
+    std::cout << "* Getting sequence member info..." << std::endl;
+    const DynamicDataInfo& info = sequence_member.info();
+    int seq_len = info.member_count();
+    std::cout << "* Sequence contains " << seq_len << " elements" << std::endl;
+
+    for (int i = 0; i < seq_len; i++) {
+        // The same results can be obtained using bind/loan method.
+        // The main difference, is that in that case the members are not copied,
+        // just referenced.
+
+        // Bind/Loan:
+        // The destructor will unloan the member.
+        DynamicData struct_element = sequence_member.loan_value(i + 1).get();
+
+        // Get:
+        struct_element = sequence_member.value<DynamicData>(i + 1);
+
+        std::cout << "Reading sequence element #" << (i + 1) << " :"
+                  << std::endl << struct_element;
+    }
 }
 
 int main()
@@ -99,6 +125,7 @@ int main()
 
         std::cout << "***** Writing a sample *****" << std::endl;
         write_data(struct_sequence_data);
+        std::cout << std::endl;
 
         std::cout << "***** Reading a sample *****" << std::endl;
         read_data(struct_sequence_data);
