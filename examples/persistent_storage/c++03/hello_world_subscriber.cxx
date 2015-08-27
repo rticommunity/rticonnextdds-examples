b610d237dd707026af0f738e1535e7cb2f68056a
Benito Palacios Sánchez <benito@rti.com>
2015-08-27 19:49:03 +0200

    Persistent: Implement c++03 subscriber

--- examples/persistent_storage/c++03/hello_world_subscriber.cxx
+++ examples/persistent_storage/c++03/hello_world_subscriber.cxx
@@ -10,19 +10,39 @@
  ******************************************************************************/
 
 #include <cstdlib>
+#include <algorithm>
 #include <iostream>
 
 #include <dds/dds.hpp>
+#include <rti/core/ListenerBinder.hpp>
 #include "hello_world.hpp"
 
 using namespace dds::core;
+using namespace dds::core::status;
 using namespace dds::domain;
 using namespace dds::topic;
-using namespace dds::pub;
-using namespace dds::pub::qos;
+using namespace dds::sub;
+using namespace dds::sub::qos;
 
-void publisher_main(int domain_id, int sample_count, int initial_value,
-    bool dwh, int sleep)
+class hello_worldReaderListener : public NoOpDataReaderListener<hello_world> {
+  public:
+    void on_data_available(DataReader<hello_world>& reader)
+    {
+        // Take all samples
+        LoanedSamples<hello_world> samples = reader.take();
+
+        for (LoanedSamples<hello_world>::iterator sample_it = samples.begin();
+            sample_it != samples.end();
+            sample_it++) {
+
+            if (sample_it->info().valid()){
+                std::cout << sample_it->data() << std::endl;
+            }
+        }
+    }
+};
+
+void subscriber_main(int domain_id, int sample_count, bool drs)
 {
     // Create a DomainParticipant with default Qos
     DomainParticipant participant(domain_id);
@@ -30,32 +50,31 @@ void publisher_main(int domain_id, int sample_count, int initial_value,
     // Create a Topic -- and automatically register the type
     Topic<hello_world> topic(participant, "Example hello_world");
 
-    // If you use Durable Writer History, you need to set several properties.
-    // These properties are set in the USER_QOS_PROFILE.xml file,
-    // "durable_writer_history_Profile" profile. See that file for further
-    // details.
+    // If you use Durable Reader State, you need to set up several properties.
+    // In this example, we have modified them using a QoS XML profile. See
+    // further details in USER_QOS_PROFILES.xml.
     std::string qos_libname = "persistence_example_Library";
-    DataWriterQos writer_qos = dwh ?
-        QosProvider::Default().datawriter_qos(
-            qos_libname + "::durable_writer_history_Profile") :
-        QosProvider::Default().datawriter_qos(
+    DataReaderQos reader_qos = drs ?
+        QosProvider::Default().datareader_qos(
+            qos_libname + "::durable_reader_state_Profile") :
+        QosProvider::Default().datareader_qos(
             qos_libname + "::persistence_service_Profile");
 
-    // Create a DataWriter (Publisher created in-line)
-    DataWriter<hello_world> writer(Publisher(participant), topic, writer_qos);
+    // Create a DataReader (Subscriber created in-line)
+    DataReader<hello_world> reader(Subscriber(participant), topic, reader_qos);
 
-    hello_world sample;
-    for (int count = 0; count < sample_count || sample_count == 0; count++) {
-        std::cout << "Writing hello_world, count " << count << std::endl;
-        sample.data(initial_value++);
-        writer.write(sample);
+    // Create a data reader listener using ListenerBinder, a RAII that
+    // will take care of setting it to NULL on destruction.
+    rti::core::ListenerBinder< DataReader<hello_world> > scoped_listener =
+        rti::core::bind_and_manage_listener(
+            reader,
+            new hello_worldReaderListener,
+            StatusMask::data_available());
 
-        rti::util::sleep(Duration(1));
-    }
-
-    while (sleep != 0) {
-        rti::util::sleep(Duration(1));
-        sleep--;
+    for (int count = 0; sample_count == 0 || count < sample_count; ++count) {
+        std::cout << "hello_world subscriber sleeping for 4 sec..."
+                  << std::endl;
+        rti::util::sleep(Duration(4));
     }
 }
 
@@ -63,33 +82,23 @@ int main(int argc, char *argv[])
 {
     int domain_id = 0;
     int sample_count = 0; // Infinite loop
-    int initial_value = 0;
-    bool dwh = false;
-    int sleep = 0;
+    bool drs = false;
 
     for (int i = 1; i < argc; ) {
         const std::string& param = argv[i++];
 
-        if (param == "-sleep" && i < argc) {
-            sleep = atoi(argv[i++]);
-        } else if (param == "-domain_id" && i < argc) {
+        if (param == "-domain_id" && i < argc) {
             domain_id = atoi(argv[i++]);
         } else if (param == "-sample_count" && i < argc) {
             sample_count = atoi(argv[i++]);
-        } else if (param == "-initial_value" && i < argc) {
-            initial_value = atoi(argv[i++]);
-        } else if (param == "-dwh" && i < argc) {
-            dwh = (atoi(argv[i++]) == 1);
+        } else if (param == "-drs" && i < argc) {
+            drs = (atoi(argv[i++]) == 1);
         } else {
             std::cout << argv[0] << " [options]" << std::endl
                       << "\t-domain_id <domain ID> (default: 0)" << std::endl
                       << "\t-sample_count <number of published samples> "
                       << "(default: 0 (infinite))" << std::endl
-                      << "\t-initial_value <first sample value> (default: 0)"
-                      << std::endl
-                      << "\t-sleep <sleep time in seconds before finishing> "
-                      << "(default: 0)" << std::endl
-                      << "\t-dwh <1|0> Enable/Disable durable writer history "
+                      << "\t-drs <1|0> Enable/Disable durable reader state "
                       << "(default: 0)" << std::endl;
             return -1;
         }
@@ -100,7 +109,7 @@ int main(int argc, char *argv[])
     // rti::config::Logger::instance().verbosity(rti::config::Verbosity::STATUS_ALL);
 
     try {
-        publisher_main(domain_id, sample_count, initial_value, dwh, sleep);
+        subscriber_main(domain_id, sample_count, drs);
     } catch (const std::exception& ex) {
         // This will catch DDS exceptions
         std::cerr << "Exception in publisher_main: " << ex.what() << std::endl;
