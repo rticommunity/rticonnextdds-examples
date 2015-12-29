77d02ef107b21c7554d848bd756630934ccb7c0d
Alex Campos <alejandro@rti.com>
2015-12-28 19:25:33 -0800

    Added copyright notice

--- examples/lambda_content_filter/c++11/LambdaFilter.hpp
+++ examples/lambda_content_filter/c++11/LambdaFilter.hpp
@@ -1,4 +1,13 @@
-// RTI Example
+/*******************************************************************************
+ (c) 2005-2015 Copyright, Real-Time Innovations, Inc.  All rights reserved.
+ RTI grants Licensee a license to use, modify, compile, and create derivative
+ works of the Software.  Licensee has the right to distribute object form only
+ for use with RTI products.  The Software is provided "as is", with no warranty
+ of any type, including any warranty for fitness for any purpose. RTI is under
+ no obligation to maintain or support the Software.  RTI shall not be liable for
+ any incidental or consequential damages arising out of the use or inability to
+ use the software.
+ ******************************************************************************/
 
 #include <dds/topic/Filter.hpp>
 #include <rti/topic/ContentFilter.hpp>
@@ -96,5 +105,3 @@ dds::topic::ContentFilteredTopic<T> create_lambda_cft(
         name,
         create_lambda_filter<T>(name, topic.participant(), lambda_func));
 }
-
-
