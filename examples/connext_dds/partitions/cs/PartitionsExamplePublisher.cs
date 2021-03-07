72a4f661729fa13926745e8d85c002d2ff488777
Alex Campos <alejandro@rti.com>
2021-03-06 17:38:29 -0800

    Rewrite Partitions c# example using new c# API

---
+++ examples/connext_dds/partitions/cs/PartitionsExamplePublisher.cs
@@ -0,0 +1,112 @@
+/*
+* (c) Copyright, Real-Time Innovations, 2021.  All rights reserved.
+* RTI grants Licensee a license to use, modify, compile, and create derivative
+* works of the software solely for use with RTI Connext DDS. Licensee may
+* redistribute copies of the software provided that all such copies are subject
+* to this license. The software is provided "as is", with no warranty of any
+* type, including any warranty for fitness for any purpose. RTI is under no
+* obligation to maintain or support the software. RTI shall not be liable for
+* any incidental or consequential damages arising out of the use or inability
+* to use the software.
+*/
+
+using System;
+using System.Threading;
+using Rti.Dds.Domain;
+using Rti.Dds.Publication;
+using Rti.Dds.Topics;
+
+namespace PartitionsExample
+{
+    /// <summary>
+    /// Example application that publishes PartitionsExample.HelloWorld.
+    /// </summary>
+    public static class HelloWorldPublisher
+    {
+        /// <summary>
+        /// Runs the publisher example.
+        /// </summary>
+        public static void RunPublisher(int domainId, int sampleCount)
+        {
+            // A DomainParticipant allows an application to begin communicating in
+            // a DDS domain. Typically there is one DomainParticipant per application.
+            // DomainParticipant QoS is configured in USER_QOS_PROFILES.xml
+            //
+            // A participant needs to be Disposed to release middleware resources.
+            // The 'using' keyword indicates that it will be Disposed when this
+            // scope ends.
+            using DomainParticipant participant = DomainParticipantFactory.Instance.CreateParticipant(domainId);
+
+            // A Topic has a name and a datatype.
+            Topic<HelloWorld> topic = participant.CreateTopic<HelloWorld>("Example PartitionsExample_HelloWorld");
+
+            // A Publisher allows an application to create one or more DataWriters
+            // Publisher QoS is configured in USER_QOS_PROFILES.xml
+            Publisher publisher = participant.CreatePublisher();
+
+            // This DataWriter will write data on Topic "Example HelloWorld"
+            // DataWriter QoS is configured in USER_QOS_PROFILES.xml
+            DataWriter<HelloWorld> writer = publisher.CreateDataWriter(topic);
+
+            var sample = new HelloWorld();
+            for (int count = 0; count < sampleCount; count++)
+            {
+                // Modify the data to be sent here
+                sample.x = count;
+
+                Console.WriteLine($"Writing HelloWorld, count {count}");
+
+                writer.Write(sample);
+
+                // Every 5 samples we will change the partition name.
+                // These are the partition expressions we are going to try:
+                // "bar", "A*", "A?C", "X*Z", "zzz" and "A*C".
+                string[] newPartitions = null;
+                if ((count + 1) % 25 == 0)
+                {
+                    // Matches "ABC", name[1] here can match name[0] there,
+                    // as long as there is some overlapping name.
+                    newPartitions = new string[] { "zzz", "A*C" };
+                }
+                else if ((count + 1) % 25 == 20)
+                {
+                    // Strings that are regular expressions aren't tested for
+                    // literal matches, so this won't match "X*Z".
+                    newPartitions = new string[] { "X*Z" };
+                }
+                else if ((count + 1) % 25 == 15)
+                {
+                    // Matches "ABC".
+                    newPartitions = new string[] { "A?C" };
+                }
+                else if ((count + 1) % 25 == 10)
+                {
+                    // Matches "ABC".
+                    newPartitions = new string[] { "A*" };
+                }
+                else if ((count + 1) % 25 == 5)
+                {
+                    // No literal match for "bar".
+                    // For the next iterations we are using only one partition.
+                    newPartitions = new string[] { "bar" };
+                }
+
+                if (newPartitions != null)
+                {
+                    // Update the publisher QoS with the new partitions
+                    publisher.Qos = publisher.Qos.WithPartition(currentPartition =>
+                    {
+                        currentPartition.Clear(); // remove previous partitions
+                        currentPartition.AddRange(newPartitions); // add new partitions
+                    });
+
+                    Console.WriteLine(
+                        "Publisher partition set to: "
+                        + string.Join(separator: ",", values: publisher.Qos.Partition.Name));
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+} // namespace PartitionsExample
---
+++ examples/connext_dds/partitions/cs/PartitionsExampleSubscriber.cs
@@ -0,0 +1,104 @@
+/*
+* (c) Copyright, Real-Time Innovations, 2012.  All rights reserved.
+* RTI grants Licensee a license to use, modify, compile, and create derivative
+* works of the software solely for use with RTI Connext DDS. Licensee may
+* redistribute copies of the software provided that all such copies are subject
+* to this license. The software is provided "as is", with no warranty of any
+* type, including any warranty for fitness for any purpose. RTI is under no
+* obligation to maintain or support the software. RTI shall not be liable for
+* any incidental or consequential damages arising out of the use or inability
+* to use the software.
+*/
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Omg.Dds.Core.Policy;
+using Omg.Dds.Subscription;
+using Rti.Dds.Core;
+using Rti.Dds.Core.Policy;
+using Rti.Dds.Domain;
+using Rti.Dds.Subscription;
+using Rti.Dds.Topics;
+
+namespace PartitionsExample
+{
+    /// <summary>
+    /// Example application that publishes PartitionsExample.HelloWorld.
+    /// </summary>
+    public static class HelloWorldSubscriber
+    {
+        /// <summary>
+        /// Runs the subscriber example.
+        /// </summary>
+        public static async Task RunSubscriber(int domainId = 0, int sampleCount = int.MaxValue, bool useXmlQos = true)
+        {
+            // A DomainParticipant allows an application to begin communicating in
+            // a DDS domain. Typically there is one DomainParticipant per application.
+            // DomainParticipant QoS is configured in USER_QOS_PROFILES.xml
+            //
+            // A participant needs to be Disposed to release middleware resources.
+            // The 'using' keyword indicates that it will be Disposed when this
+            // scope ends.
+            using DomainParticipant participant = DomainParticipantFactory.Instance.CreateParticipant(domainId);
+
+            // A Topic has a name and a datatype.
+            Topic<HelloWorld> topic = participant.CreateTopic<HelloWorld>("Example PartitionsExample_HelloWorld");
+
+            // Configure the Subscriber's Partition QoS policy.
+            SubscriberQos subscriberQos;
+            if (useXmlQos)
+            {
+                // Retrieve the default Subscriber QoS, from USER_QOS_PROFILES.xml
+                subscriberQos = QosProvider.Default.GetSubscriberQos();
+            }
+            else
+            {
+                subscriberQos = participant.DefaultSubscriberQos.WithPartition(p =>
+                {
+                    p.Add("ABC");
+                    p.Add("X*Z");
+                });
+            }
+
+            Subscriber subscriber = participant.CreateSubscriber(subscriberQos);
+
+            Console.WriteLine(
+                "Subscriber partition set to: " 
+                + string.Join(separator: ",", values: subscriber.Qos.Partition.Name));
+
+            DataReaderQos readerQos;
+            if (useXmlQos)
+            {
+                // Retrieve the default DataReader QoS, from USER_QOS_PROFILES.xml
+                readerQos = QosProvider.Default.GetDataReaderQos();
+            }
+            else
+            {
+                readerQos = subscriber.DefaultDataReaderQos
+                    .WithReliability(policy => policy.Kind = ReliabilityKind.Reliable)
+                    .WithHistory(policy => policy.Kind = HistoryKind.KeepAll)
+                    .WithDurability(policy => policy.Kind = DurabilityKind.TransientLocal);
+            }
+
+            DataReader<HelloWorld> reader = subscriber.CreateDataReader(topic, readerQos);
+
+            // Take all samples as they are received. Stop after taking sampleCount samples.
+            await foreach (var sample in reader.TakeAsync().Take(sampleCount))
+            {
+                if (sample.Info.ValidData)
+                {
+                    if (sample.Info.State.View == ViewState.New)
+                    {
+                        // When we receive a new instance because a partition
+                        // update made it possible, the View state is
+                        // ViewState.New
+                        Console.WriteLine("Found new instance");
+                    }
+
+                    Console.WriteLine(sample.Data);
+                }
+            }
+        }
+    }
+} // namespace PartitionsExample
