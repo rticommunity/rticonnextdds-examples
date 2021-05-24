dab80923346bb7d6ed405e1836b76e0180e0750c
Manuel Jesús Núñez Ruiz <manueljesusnunezruiz@gmail.com>
2021-05-24 17:56:08 +0200

    Divide the linux_static_analysis script (#443)
    * remove build commands from linux_static_analysis
    
    * add a new file with the build commands
    
    * adapt GH Actions workflow
    
    * apply PR feedback
    
    * add support for RTI_INSTALLATION_PATH env variable
    
    * remove unused variable
    
    * update linux_static_analysis to use RTI_INSTALLATION_PATH
    
    * convert the environment variable in path
    
    * fix connextdds finding
    
    * Remove RTI_PACKAGE_VERSION environment variable
    
    * remove RTI_PACKAGE_VERSION from the build workflow
    
    * Update resources/ci_cd/linux_build.py
    
    Co-authored-by: Luvo <luislivilla@gmail.com>
    
    * Update resources/ci_cd/linux_build.py
    
    Co-authored-by: Luvo <luislivilla@gmail.com>
    
    * Update resources/ci_cd/linux_build.py
    
    Co-authored-by: Luvo <luislivilla@gmail.com>
    
    * Update resources/ci_cd/linux_static_analysis.py
    
    Co-authored-by: Luvo <luislivilla@gmail.com>
    
    * Update resources/ci_cd/linux_static_analysis.py
    
    Co-authored-by: Luvo <luislivilla@gmail.com>
    
    * Update resources/ci_cd/linux_static_analysis.py
    
    Co-authored-by: Luvo <luislivilla@gmail.com>
    
    * Update resources/ci_cd/linux_build.py
    
    Co-authored-by: Luvo <luislivilla@gmail.com>
    
    * fix code formatter issue
    
    * fix condition
    
    * refactor linux_install
    
    Co-authored-by: Luvo <luislivilla@gmail.com>

--- resources/ci_cd/linux_build.py
+++ resources/ci_cd/linux_build.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# (c) 2019 Copyright, Real-Time Innovations, Inc.  All rights reserved. RTI
+# (c) 2021 Copyright, Real-Time Innovations, Inc.  All rights reserved. RTI
 # grants Licensee a license to use, modify, compile, and create derivative
 # works of the Software.  Licensee has the right to distribute object form only
 # for use with RTI products.  The Software is provided "as is", with no
@@ -8,17 +8,16 @@
 # is under no obligation to maintain or support the Software.  RTI shall not be
 # liable for any incidental or consequential damages arising out of the use or
 # inability to use the software.
-"""Run static analysis over all the built examples.
+"""Build all the examples.
 
 The environment variable RTI_PACKAGE_VERSION must be assigned to find the
-correct RTIConnextDDS package version. Also, the examples must be compiled
-to be able to run the static analysis.
+correct RTIConnextDDS package version.
 
 """
-
 import os
 import sys
 import subprocess
+
 from pathlib import Path
 
 
@@ -31,12 +30,9 @@ def main():
         sys.exit("The RTI_INSTALLATION_PATH does not exist.")
 
     try:
-        build_dir = Path("examples/connext_dds/build").resolve(strict=True)
+        examples_dir = Path("examples/connext_dds").resolve(strict=True)
     except FileNotFoundError:
-        sys.exit(
-            "Error: build directory not found, compile the examples before "
-            "running this script."
-        )
+        sys.exit("Error: Examples directory not found.")
 
     found_rti_connext_dds = list(
         rti_installation_path.glob("rti_connext_dds-?.?.?")
@@ -46,31 +42,37 @@ def main():
         sys.exit("Error: RTIConnextDDS not found.")
 
     rti_connext_dds_dir = found_rti_connext_dds[0]
+    build_dir = examples_dir.joinpath("build")
 
-    try:
-        rti_connext_dds_include_dir = rti_connext_dds_dir.joinpath(
-            "include"
-        ).resolve(strict=True)
-    except FileNotFoundError:
-        sys.exit("Error: RTIConnextDDS not found.")
+    build_dir.mkdir(exist_ok=True)
 
-    static_analysis_result = subprocess.run(
+    print("[RTICommunity] Generating build system...", flush=True)
+
+    build_gen_result = subprocess.run(
         [
-            "analyze-build",
-            "--verbose",
-            "--status-bugs",
-            "--exclude",
-            rti_connext_dds_include_dir,
-            "--exclude",
-            ".",
-            "-o",
-            ".",
+            "cmake",
+            "-DSTATIC_ANALYSIS=ON",
+            "-DBUILD_SHARED_LIBS=ON",
+            "-DCMAKE_BUILD_TYPE=Release",
+            "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON",
+            f"-DCONNEXTDDS_DIR={rti_connext_dds_dir}",
+            examples_dir,
         ],
         cwd=build_dir,
     )
 
-    if static_analysis_result.returncode != 0:
-        sys.exit("There where some errors during static analysis.")
+    if build_gen_result.returncode:
+        sys.exit("There was some errors during generating the build system.")
+
+    print("\n[RTICommunity] Compiling the examples...", flush=True)
+
+    building_result = subprocess.run(
+        ["cmake", "--build", ".", "--config", "Release"],
+        cwd=build_dir,
+    )
+
+    if building_result.returncode:
+        sys.exit("There was some errors during build.")
 
 
 if __name__ == "__main__":
