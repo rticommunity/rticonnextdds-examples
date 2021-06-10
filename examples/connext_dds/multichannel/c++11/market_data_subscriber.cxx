96244f1258a2d48ca61a7acb557c83755f59b167
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-06-10 13:55:57 +0200

    update multichannel C++11 template

--- examples/connext_dds/multichannel/c++11/market_data_subscriber.cxx
+++ examples/connext_dds/multichannel/c++11/market_data_subscriber.cxx
@@ -9,109 +9,119 @@
  use the software.
  ******************************************************************************/
 
-#include <algorithm>
-#include <iostream>
+#include <dds/sub/ddssub.hpp>
+#include <dds/core/ddscore.hpp>
+#include <rti/config/Logger.hpp>  // for logging
 
 #include "market_data.hpp"
-#include <dds/dds.hpp>
-#include <rti/core/ListenerBinder.hpp>
+#include "application.hpp"  // for command line parsing and ctrl-c
 
-using namespace dds::core;
-using namespace rti::core;
-using namespace dds::core::status;
-using namespace dds::domain;
-using namespace dds::sub;
-using namespace dds::topic;
+int process_data(dds::sub::DataReader<market_data> reader)
+{
+    int count = 0;
+    // Take all samples
+    dds::sub::LoanedSamples<market_data> samples = reader.take();
 
-class MarketDataReaderListener : public NoOpDataReaderListener<market_data> {
-public:
-    void on_data_available(DataReader<market_data> &reader)
-    {
-        // Take all samples
-        LoanedSamples<market_data> samples = reader.take();
-
-        for (const auto &sample : samples) {
-            if (sample.info().valid()) {
-                std::cout << sample.data() << std::endl;
-            }
+    for (const auto &sample : samples) {
+        if (sample.info().valid()) {
+            count++;
+            std::cout << sample.data() << std::endl;
         }
     }
-};
 
-void subscriber_main(int domain_id, int sample_count)
+    return count;
+}
+
+void run_subscriber_application(
+        unsigned int domain_id,
+        unsigned int sample_count)
 {
     // Create a DomainParticipant with default Qos
-    DomainParticipant participant(domain_id);
+    dds::domain::DomainParticipant participant(domain_id);
 
     // Create a Topic -- and automatically register the type
-    Topic<market_data> topic(participant, "Example market_data");
+    dds::topic::Topic<market_data> topic(participant, "Example market_data");
 
     // Create a ContentFiltered Topic and specify the STRINGMATCH filter name
     // to use a built-in filter for matching multiple strings.
     // More information can be found in the example
     // 'content_filtered_topic_string_filter'.
-    Filter filter("Symbol MATCH 'A'", std::vector<std::string>());
+    dds::topic::Filter filter("Symbol MATCH 'A'", std::vector<std::string>());
     filter->name(rti::topic::stringmatch_filter_name());
     std::cout << "filter is " << filter.expression() << std::endl;
 
-    ContentFilteredTopic<market_data> cft_topic(
+    dds::topic::ContentFilteredTopic<market_data> cft_topic(
             topic,
             "ContentFilteredTopic",
             filter);
 
-    // Create a DataReader with default Qos (Subscriber created in-line)
-    DataReader<market_data> reader(Subscriber(participant), cft_topic);
+    // Create a subscriber with default QoS
+    dds::sub::Subscriber subscriber(participant);
 
-    // Create a data reader listener using ListenerBinder, a RAII that
-    // will take care of setting it to NULL on destruction.
-    rti::core::ListenerBinder<DataReader<market_data>> scoped_listener =
-            rti::core::bind_and_manage_listener(
-                    reader,
-                    new MarketDataReaderListener,
-                    StatusMask::data_available());
+    // Create a DataReader with default Qos (Subscriber created in-line)
+    dds::sub::DataReader<market_data> reader(subscriber, cft_topic);
+
+    // WaitSet will be woken when the attached condition is triggered
+    dds::core::cond::WaitSet waitset;
+
+    // Create a ReadCondition for any data on this reader, and add to WaitSet
+    unsigned int samples_read = 0;
+    dds::sub::cond::ReadCondition read_condition(
+            reader,
+            dds::sub::status::DataState::any(),
+            [reader, &samples_read]() {
+                // If we wake up, process data
+                samples_read += process_data(reader);
+            });
+
+    waitset += read_condition;
 
     // Main loop.
-    for (int count = 0; sample_count == 0 || count < sample_count; count++) {
-        if (count == 3) {
+    while (!application::shutdown_requested && samples_read < sample_count) {
+        if (samples_read == 3) {
             // On t=3 we add the symbol 'D' to the filter parameter
             // to match 'A' and 'D'.
             cft_topic->append_to_expression_parameter(0, "D");
             std::cout << "changed filter to Symbol MATCH 'A,D'" << std::endl;
-        } else if (count == 6) {
+        } else if (samples_read == 6) {
             // On t=6 we remove the symbol 'A' to the filter parameter
             // to match only 'D'.
             cft_topic->remove_from_expression_parameter(0, "A");
             std::cout << "changed filter to Symbol MATCH 'D'" << std::endl;
         }
 
-        rti::util::sleep(Duration(4));
+        waitset.dispatch(dds::core::Duration(4));
     }
 }
 
 int main(int argc, char *argv[])
 {
-    int domain_id = 0;
-    int sample_count = 0;  // Infinite loop
+    using namespace application;
 
-    if (argc >= 2) {
-        domain_id = atoi(argv[1]);
+    // Parse arguments and handle control-C
+    auto arguments = parse_arguments(argc, argv);
+    if (arguments.parse_result == ParseReturn::exit) {
+        return EXIT_SUCCESS;
+    } else if (arguments.parse_result == ParseReturn::failure) {
+        return EXIT_FAILURE;
     }
+    setup_signal_handlers();
 
-    if (argc >= 3) {
-        sample_count = atoi(argv[2]);
-    }
-
-    // To turn on additional logging, include <rti/config/Logger.hpp> and
-    // uncomment the following line:
-    // rti::config::Logger::instance().verbosity(rti::config::Verbosity::STATUS_ALL);
+    // Sets Connext verbosity to help debugging
+    rti::config::Logger::instance().verbosity(arguments.verbosity);
 
     try {
-        subscriber_main(domain_id, sample_count);
+        run_subscriber_application(arguments.domain_id, arguments.sample_count);
     } catch (const std::exception &ex) {
         // This will catch DDS exceptions
-        std::cerr << "Exception in subscriber_main: " << ex.what() << std::endl;
-        return -1;
+        std::cerr << "Exception in run_subscriber_application(): " << ex.what()
+                  << std::endl;
+        return EXIT_FAILURE;
     }
 
-    return 0;
+    // Releases the memory used by the participant factory.  Optional at
+    // application exit
+    dds::domain::DomainParticipant::finalize_participant_factory();
+
+    return EXIT_SUCCESS;
 }
