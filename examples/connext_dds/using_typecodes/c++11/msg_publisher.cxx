9414a676fe4c22364b7a22146ac823cf75d8e919
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-06-14 09:32:21 +0200

    update using_typecodes C++11 template

--- examples/connext_dds/using_typecodes/c++11/msg_publisher.cxx
+++ examples/connext_dds/using_typecodes/c++11/msg_publisher.cxx
@@ -9,24 +9,20 @@
  use the software.
  ******************************************************************************/
 
-#include <cstdlib>
-#include <iostream>
+#include <dds/pub/ddspub.hpp>
+#include <rti/util/util.hpp>      // for sleep()
+#include <rti/config/Logger.hpp>  // for logging
 
+#include "application.hpp"  // for command line parsing and ctrl-c
 #include "msg.hpp"
-#include <dds/dds.hpp>
 
-using namespace dds::core;
-using namespace rti::core::policy;
-using namespace dds::domain;
-using namespace dds::domain::qos;
-using namespace dds::topic;
-using namespace dds::pub;
-
-void publisher_main(int domain_id, int sample_count)
+void run_publisher_application(
+        unsigned int domain_id,
+        unsigned int sample_count)
 {
     // Retrieve the Participant QoS, from USER_QOS_PROFILES.xml
-    DomainParticipantQos participant_qos =
-            QosProvider::Default()->participant_qos();
+    dds::domain::qos::DomainParticipantQos participant_qos =
+            dds::core::QosProvider::Default()->participant_qos();
 
     // If you want to change the Participant's QoS programmatically rather than
     // using the XML file, uncomment the following lines.
@@ -35,49 +31,58 @@ void publisher_main(int domain_id, int sample_count)
     //     type_code_max_serialized_length(3070);
 
     // Create a DomainParticipant.
-    DomainParticipant participant(domain_id, participant_qos);
+    dds::domain::DomainParticipant participant(domain_id, participant_qos);
 
     // Create a Topic -- and automatically register the type
-    Topic<msg> topic(participant, "Example msg");
+    dds::topic::Topic<msg> topic(participant, "Example msg");
 
-    // Create a DataWriter with default Qos (Publisher created in-line)
-    DataWriter<msg> writer(Publisher(participant), topic);
+    // Create a Publisher with default QoS
+    dds::pub::Publisher publisher(participant);
+
+    // Create a DataWriter with default Qos
+    dds::pub::DataWriter<msg> writer(publisher, topic);
 
     msg sample;
-    for (int count = 0; count < sample_count || sample_count == 0; count++) {
+    for (unsigned int samples_written = 0;
+         !application::shutdown_requested && samples_written < sample_count;
+         samples_written++) {
         // Update sample and send it.
-        std::cout << "Writing msg, count " << count << std::endl;
-        sample.count(count);
+        std::cout << "Writing msg, count " << samples_written << std::endl;
+        sample.count(samples_written);
         writer.write(sample);
 
-        rti::util::sleep(Duration(4));
+        rti::util::sleep(dds::core::Duration(4));
     }
 }
 
 int main(int argc, char *argv[])
 {
-    int domain_id = 0;
-    int sample_count = 0;  // Infinite loop
+    using namespace application;
 
-    if (argc >= 2) {
-        domain_id = atoi(argv[1]);
+    // Parse arguments and handle control-C
+    auto arguments = parse_arguments(argc, argv);
+    if (arguments.parse_result == ParseReturn::exit) {
+        return EXIT_SUCCESS;
+    } else if (arguments.parse_result == ParseReturn::failure) {
+        return EXIT_FAILURE;
     }
+    setup_signal_handlers();
 
-    if (argc >= 3) {
-        sample_count = atoi(argv[2]);
-    }
-
-    // To turn on additional logging, include <rti/config/Logger.hpp> and
-    // uncomment the following line:
-    // rti::config::Logger::instance().verbosity(rti::config::Verbosity::STATUS_ALL);
+    // Sets Connext verbosity to help debugging
+    rti::config::Logger::instance().verbosity(arguments.verbosity);
 
     try {
-        publisher_main(domain_id, sample_count);
+        run_publisher_application(arguments.domain_id, arguments.sample_count);
     } catch (const std::exception &ex) {
         // This will catch DDS exceptions
-        std::cerr << "Exception in publisher_main: " << ex.what() << std::endl;
-        return -1;
+        std::cerr << "Exception in run_publisher_application(): " << ex.what()
+                  << std::endl;
+        return EXIT_FAILURE;
     }
 
-    return 0;
+    // Releases the memory used by the participant factory.  Optional at
+    // application exit
+    dds::domain::DomainParticipant::finalize_participant_factory();
+
+    return EXIT_SUCCESS;
 }
--- examples/connext_dds/using_typecodes/c++11/msg_subscriber.cxx
+++ examples/connext_dds/using_typecodes/c++11/msg_subscriber.cxx
@@ -9,66 +9,60 @@
  use the software.
  ******************************************************************************/
 
-#include <algorithm>
-#include <cstdlib>
-#include <iomanip>
-#include <iostream>
+#include <dds/sub/ddssub.hpp>
+#include <dds/core/ddscore.hpp>
+#include <rti/config/Logger.hpp>  // for logging
 
 #include "msg.hpp"
-#include <dds/dds.hpp>
-#include <rti/core/ListenerBinder.hpp>
+#include "application.hpp"  // for command line parsing and ctrl-c
 
-using namespace dds::core;
-using namespace rti::core::policy;
-using namespace dds::domain;
-using namespace dds::domain::qos;
-using namespace dds::topic;
-using namespace dds::sub;
+int process_data(
+        dds::sub::DataReader<dds::topic::PublicationBuiltinTopicData> reader)
+{
+    int count = 0;
+    // Take all samples
+    dds::sub::LoanedSamples<dds::topic::PublicationBuiltinTopicData> samples =
+            reader.take();
 
-class BuiltinPublicationListener
-        : public NoOpDataReaderListener<PublicationBuiltinTopicData> {
-public:
-    void on_data_available(DataReader<PublicationBuiltinTopicData> &reader)
-    {
-        // Take all samples
-        LoanedSamples<PublicationBuiltinTopicData> samples = reader.take();
+    for (const auto &sample : samples) {
+        if (sample.info().valid()) {
+            const dds::topic::PublicationBuiltinTopicData &data = sample.data();
+            const dds::topic::BuiltinTopicKey &partKey = data.participant_key();
+            const dds::topic::BuiltinTopicKey &key = sample.data().key();
 
-        for (const auto &sample : samples) {
-            if (sample.info().valid()) {
-                const PublicationBuiltinTopicData &data = sample.data();
-                const BuiltinTopicKey &partKey = data.participant_key();
-                const BuiltinTopicKey &key = sample.data().key();
+            std::cout << std::hex << std::setw(8) << std::setfill('0');
+            std::cout << "-----" << std::endl
+                      << "Found topic \"" << data.topic_name() << "\""
+                      << std::endl
+                      << "participant: " << partKey.value()[0]
+                      << partKey.value()[1] << partKey.value()[2] << std::endl
+                      << "datawriter:  " << key.value()[0] << key.value()[1]
+                      << key.value()[2] << std::endl
+                      << "type:" << std::endl;
 
-                std::cout << std::hex << std::setw(8) << std::setfill('0');
-                std::cout << "-----" << std::endl
-                          << "Found topic \"" << data.topic_name() << "\""
-                          << std::endl
-                          << "participant: " << partKey.value()[0]
-                          << partKey.value()[1] << partKey.value()[2]
-                          << std::endl
-                          << "datawriter:  " << key.value()[0] << key.value()[1]
-                          << key.value()[2] << std::endl
-                          << "type:" << std::endl;
-
-                if (!data->type().is_set()) {
-                    std::cout << "No type received, perhaps increase "
-                              << "type_code_max_serialized_length?"
-                              << std::endl;
-                } else {
-                    // Using the type propagated we print the data type
-                    // with print_idl()
-                    rti::core::xtypes::print_idl(data->type().get(), 2);
-                }
+            if (!data->type().is_set()) {
+                std::cout << "No type received, perhaps increase "
+                          << "type_code_max_serialized_length?" << std::endl;
+            } else {
+                // Using the type propagated we print the data type
+                // with print_idl()
+                rti::core::xtypes::print_idl(data->type().get(), 2);
             }
+
+            count++;
         }
     }
-};
 
-void subscriber_main(int domain_id, int sample_count)
+    return count;
+}
+
+void run_subscriber_application(
+        unsigned int domain_id,
+        unsigned int sample_count)
 {
     // Retrieve the Participant QoS, from USER_QOS_PROFILES.xml
-    DomainParticipantQos participant_qos =
-            QosProvider::Default()->participant_qos();
+    dds::domain::qos::DomainParticipantQos participant_qos =
+            dds::core::QosProvider::Default()->participant_qos();
 
     // If you want to change the Participant's QoS programmatically rather than
     // using the XML file, uncomment the following lines.
@@ -77,55 +71,67 @@ void subscriber_main(int domain_id, int sample_count)
     //     type_code_max_serialized_length(3070);
 
     // Create a DomainParticipant.
-    DomainParticipant participant(domain_id, participant_qos);
+    dds::domain::DomainParticipant participant(domain_id, participant_qos);
 
     // First get the builtin subscriber.
-    Subscriber builtin_subscriber = dds::sub::builtin_subscriber(participant);
+    dds::sub::Subscriber builtin_subscriber =
+            dds::sub::builtin_subscriber(participant);
 
     // Then get builtin subscriber's DataReader for DataWriters.
-    DataReader<PublicationBuiltinTopicData> publication_reader =
-            rti::sub::find_datareader_by_topic_name<
-                    DataReader<PublicationBuiltinTopicData>>(
+    dds::sub::DataReader<dds::topic::PublicationBuiltinTopicData>
+            publication_reader = rti::sub::find_datareader_by_topic_name<
+                    dds::sub::DataReader<
+                            dds::topic::PublicationBuiltinTopicData>>(
                     builtin_subscriber,
                     dds::topic::publication_topic_name());
 
-    // Install our listener using ListenerBinder, a RAII that will take care
-    // of setting it to NULL and deleting it.
-    rti::core::ListenerBinder<DataReader<PublicationBuiltinTopicData>>
-            publication_listener = rti::core::bind_and_manage_listener(
-                    publication_reader,
-                    new BuiltinPublicationListener,
-                    dds::core::status::StatusMask::data_available());
+    // WaitSet will be woken when the attached condition is triggered
+    dds::core::cond::WaitSet waitset;
 
-    for (int count = 0; sample_count == 0 || count < sample_count; ++count) {
-        rti::util::sleep(Duration(1));
+    // Create a ReadCondition for any data on this reader, and add to WaitSet
+    unsigned int samples_read = 0;
+    dds::sub::cond::ReadCondition read_condition(
+            publication_reader,
+            dds::sub::status::DataState::any(),
+            [publication_reader, &samples_read]() {
+                samples_read += process_data(publication_reader);
+            });
+
+    waitset += read_condition;
+
+    while (!application::shutdown_requested && samples_read < sample_count) {
+        waitset.dispatch(dds::core::Duration(1));
     }
 }
 
 int main(int argc, char *argv[])
 {
-    int domain_id = 0;
-    int sample_count = 0;  // Infinite loop
+    using namespace application;
 
-    if (argc >= 2) {
-        domain_id = atoi(argv[1]);
+    // Parse arguments and handle control-C
+    auto arguments = parse_arguments(argc, argv);
+    if (arguments.parse_result == ParseReturn::exit) {
+        return EXIT_SUCCESS;
+    } else if (arguments.parse_result == ParseReturn::failure) {
+        return EXIT_FAILURE;
     }
+    setup_signal_handlers();
 
-    if (argc >= 3) {
-        sample_count = atoi(argv[2]);
-    }
-
-    // To turn on additional logging, include <rti/config/Logger.hpp> and
-    // uncomment the following line:
-    // rti::config::Logger::instance().verbosity(rti::config::Verbosity::STATUS_ALL);
+    // Sets Connext verbosity to help debugging
+    rti::config::Logger::instance().verbosity(arguments.verbosity);
 
     try {
-        subscriber_main(domain_id, sample_count);
+        run_subscriber_application(arguments.domain_id, arguments.sample_count);
     } catch (const std::exception &ex) {
         // This will catch DDS exceptions
-        std::cerr << "Exception in subscriber_main: " << ex.what() << std::endl;
-        return -1;
+        std::cerr << "Exception in run_subscriber_application(): " << ex.what()
+                  << std::endl;
+        return EXIT_FAILURE;
     }
 
-    return 0;
+    // Releases the memory used by the participant factory.  Optional at
+    // application exit
+    dds::domain::DomainParticipant::finalize_participant_factory();
+
+    return EXIT_SUCCESS;
 }
