a8be31109aa11c49d23d14fadf505cde545a6096
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-08-04 10:55:41 +0200

    :rotating_light:

--- examples/connext_dds/detect_samples_dropped/java/DroppedSamplesExampleSubscriber.java
+++ examples/connext_dds/detect_samples_dropped/java/DroppedSamplesExampleSubscriber.java
@@ -1,14 +1,14 @@
 /*
-* (c) Copyright, Real-Time Innovations, 2021.  All rights reserved.
-* RTI grants Licensee a license to use, modify, compile, and create derivative
-* works of the software solely for use with RTI Connext DDS. Licensee may
-* redistribute copies of the software provided that all such copies are subject
-* to this license. The software is provided "as is", with no warranty of any
-* type, including any warranty for fitness for any purpose. RTI is under no
-* obligation to maintain or support the software. RTI shall not be liable for
-* any incidental or consequential damages arising out of the use or inability
-* to use the software.
-*/
+ * (c) Copyright, Real-Time Innovations, 2021.  All rights reserved.
+ * RTI grants Licensee a license to use, modify, compile, and create derivative
+ * works of the software solely for use with RTI Connext DDS. Licensee may
+ * redistribute copies of the software provided that all such copies are subject
+ * to this license. The software is provided "as is", with no warranty of any
+ * type, including any warranty for fitness for any purpose. RTI is under no
+ * obligation to maintain or support the software. RTI shall not be liable for
+ * any incidental or consequential damages arising out of the use or inability
+ * to use the software.
+ */
 
 import java.util.Objects;
 
@@ -20,25 +20,30 @@ import com.rti.dds.subscription.*;
 import com.rti.dds.topic.*;
 
 /**
-* Simple example showing all Connext code in one place for readability.
-*/
-public class DroppedSamplesExampleSubscriber extends Application implements AutoCloseable {
-
-    private DomainParticipant participant = null; // Usually one per application
+ * Simple example showing all Connext code in one place for readability.
+ */
+public class DroppedSamplesExampleSubscriber
+        extends Application implements AutoCloseable {
+    private DomainParticipant participant = null;  // Usually one per
+                                                   // application
     private DroppedSamplesExampleDataReader reader = null;
-    private final DroppedSamplesExampleSeq dataSeq = new DroppedSamplesExampleSeq();
+    private final DroppedSamplesExampleSeq dataSeq =
+            new DroppedSamplesExampleSeq();
     private final SampleInfoSeq infoSeq = new SampleInfoSeq();
 
-    private int processData() {
+    private int processData()
+    {
         int samplesRead = 0;
 
         try {
             // Take available data from DataReader's queue
-            reader.take(dataSeq, infoSeq,
-            ResourceLimitsQosPolicy.LENGTH_UNLIMITED,
-            SampleStateKind.ANY_SAMPLE_STATE,
-            ViewStateKind.ANY_VIEW_STATE,
-            InstanceStateKind.ANY_INSTANCE_STATE);
+            reader.take(
+                    dataSeq,
+                    infoSeq,
+                    ResourceLimitsQosPolicy.LENGTH_UNLIMITED,
+                    SampleStateKind.ANY_SAMPLE_STATE,
+                    ViewStateKind.ANY_VIEW_STATE,
+                    InstanceStateKind.ANY_INSTANCE_STATE);
 
             for (int i = 0; i < dataSeq.size(); ++i) {
                 SampleInfo info = infoSeq.get(i);
@@ -58,62 +63,64 @@ public class DroppedSamplesExampleSubscriber extends Application implements Auto
         return samplesRead;
     }
 
-    private void runApplication() {
+    private void runApplication()
+    {
         // Start communicating in a domain
         participant = Objects.requireNonNull(
-            DomainParticipantFactory.get_instance().create_participant(
-                getDomainId(),
-                DomainParticipantFactory.PARTICIPANT_QOS_DEFAULT,
-                null, // listener
-                StatusKind.STATUS_MASK_NONE));
+                DomainParticipantFactory.get_instance().create_participant(
+                        getDomainId(),
+                        DomainParticipantFactory.PARTICIPANT_QOS_DEFAULT,
+                        null,  // listener
+                        StatusKind.STATUS_MASK_NONE));
 
         // A Subscriber allows an application to create one or more DataReaders
-        Subscriber subscriber = Objects.requireNonNull(
-            participant.create_subscriber(
-                DomainParticipant.SUBSCRIBER_QOS_DEFAULT,
-                null, // listener
-                StatusKind.STATUS_MASK_NONE));
+        Subscriber subscriber =
+                Objects.requireNonNull(participant.create_subscriber(
+                        DomainParticipant.SUBSCRIBER_QOS_DEFAULT,
+                        null,  // listener
+                        StatusKind.STATUS_MASK_NONE));
 
         // Register the datatype to use when creating the Topic
         String typeName = DroppedSamplesExampleTypeSupport.get_type_name();
         DroppedSamplesExampleTypeSupport.register_type(participant, typeName);
 
         // Create a Topic with a name and a datatype
-        Topic topic = Objects.requireNonNull(
-            participant.create_topic(
+        Topic topic = Objects.requireNonNull(participant.create_topic(
                 "Example DroppedSamplesExample",
                 typeName,
                 DomainParticipant.TOPIC_QOS_DEFAULT,
-                null, // listener
+                null,  // listener
                 StatusKind.STATUS_MASK_NONE));
 
         StringSeq parameters = new StringSeq();
-        ContentFilteredTopic cft = Objects.requireNonNull(
-                participant.create_contentfilteredtopic(
-                    "Example DroppedSamplesExample CFT",
-                    topic,
-                    "x < 4",
-                    parameters));
+        ContentFilteredTopic cft =
+                Objects.requireNonNull(participant.create_contentfilteredtopic(
+                        "Example DroppedSamplesExample CFT",
+                        topic,
+                        "x < 4",
+                        parameters));
 
         DataReaderQos reader_qos = new DataReaderQos();
         participant.get_default_datareader_qos(reader_qos);
-        reader_qos.ownership.kind = OwnershipQosPolicyKind.EXCLUSIVE_OWNERSHIP_QOS;
+        reader_qos.ownership.kind =
+                OwnershipQosPolicyKind.EXCLUSIVE_OWNERSHIP_QOS;
 
         // This DataReader reads data on "Example DroppedSamplesExample" Topic
         reader = (DroppedSamplesExampleDataReader) Objects.requireNonNull(
-            subscriber.create_datareader(
-                cft,
-                reader_qos,
-                null, // listener
-                StatusKind.STATUS_MASK_NONE));
+                subscriber.create_datareader(
+                        cft,
+                        reader_qos,
+                        null,  // listener
+                        StatusKind.STATUS_MASK_NONE));
 
         // Create ReadCondition that triggers when data in reader's queue
         ReadCondition condition = reader.create_readcondition(
-            SampleStateKind.ANY_SAMPLE_STATE,
-            ViewStateKind.ANY_VIEW_STATE,
-            InstanceStateKind.ANY_INSTANCE_STATE);
+                SampleStateKind.ANY_SAMPLE_STATE,
+                ViewStateKind.ANY_VIEW_STATE,
+                InstanceStateKind.ANY_INSTANCE_STATE);
 
-        // WaitSet will be woken when the attached condition is triggered, or timeout
+        // WaitSet will be woken when the attached condition is triggered, or
+        // timeout
         WaitSet waitset = new WaitSet();
         waitset.attach_condition(condition);
         final Duration_t waitTimeout = new Duration_t(1, 0);
@@ -133,7 +140,9 @@ public class DroppedSamplesExampleSubscriber extends Application implements Auto
 
             } catch (RETCODE_TIMEOUT timeout) {
                 // No data received, not a problem
-                System.out.printf("No data after %d seconds.%n", waitTimeout.sec);
+                System.out.printf(
+                        "No data after %d seconds.%n",
+                        waitTimeout.sec);
             }
             reader.get_datareader_cache_status(status);
             System.out.println(
@@ -145,27 +154,31 @@ public class DroppedSamplesExampleSubscriber extends Application implements Auto
         }
     }
 
-    @Override
-    public void close() {
-        // Delete all entities (DataReader, Topic, Subscriber, DomainParticipant)
+    @Override public void close()
+    {
+        // Delete all entities (DataReader, Topic, Subscriber,
+        // DomainParticipant)
         if (participant != null) {
             participant.delete_contained_entities();
 
-            DomainParticipantFactory.get_instance()
-            .delete_participant(participant);
+            DomainParticipantFactory.get_instance().delete_participant(
+                    participant);
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // Create example and run: Uses try-with-resources,
         // subscriberApplication.close() automatically called
-        try (DroppedSamplesExampleSubscriber subscriberApplication = new DroppedSamplesExampleSubscriber()) {
+        try (DroppedSamplesExampleSubscriber subscriberApplication =
+                     new DroppedSamplesExampleSubscriber()) {
             subscriberApplication.parseArguments(args);
             subscriberApplication.addShutdownHook();
             subscriberApplication.runApplication();
         }
 
-        // Releases the memory used by the participant factory. Optional at application exit.
+        // Releases the memory used by the participant factory. Optional at
+        // application exit.
         DomainParticipantFactory.finalize_instance();
     }
 }
