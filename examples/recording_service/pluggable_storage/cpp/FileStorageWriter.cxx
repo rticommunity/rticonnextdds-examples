7c8174d6bb7947a9cb6164da8ceb18fb1c964c3d
Antonio Lopez <alopez@rti.com>
2021-11-26 10:28:28 +0100

    #477 Fixing indendation: Third round

--- examples/recording_service/pluggable_storage/cpp/FileStorageWriter.cxx
+++ examples/recording_service/pluggable_storage/cpp/FileStorageWriter.cxx
@@ -141,7 +141,8 @@ void FileStreamWriter::store(
     for (size_t i = 0; i < count; ++i) {
         const SampleInfo &sample_info = *(info_seq[i]);
         // we first first print the sample's metadata
-        int64_t timestamp = static_cast<int64_t>(sample_info->reception_timestamp().sec())
+        int64_t timestamp =
+                static_cast<int64_t>(sample_info->reception_timestamp().sec())
                 * NANOSECS_PER_SEC;
         timestamp += sample_info->reception_timestamp().nanosec();
 
@@ -183,7 +184,8 @@ void PubDiscoveryFileStreamWriter::store(
     for (size_t i = 0; i < count; ++i) {
         const SampleInfo &sample_info = *(info_seq[i]);
         // we first first print the sample's metadata
-        int64_t timestamp = static_cast<int64_t>(sample_info->reception_timestamp().sec())
+        int64_t timestamp =
+                static_cast<int64_t>(sample_info->reception_timestamp().sec())
                 * NANOSECS_PER_SEC;
         timestamp += sample_info->reception_timestamp().nanosec();
 
