75ed5a63098293d2a26a46db81efaf2b7156be82
mnunez <mnunez@rti.com>
2022-05-10 12:58:35 +0200

    fix formatting issues (fix #498)

--- examples/connext_dds/instance_statistics/c++11/instance_subscriber.cxx
+++ examples/connext_dds/instance_statistics/c++11/instance_subscriber.cxx
@@ -1,14 +1,14 @@
 /*
-* (c) Copyright, Real-Time Innovations, 2021.  All rights reserved.
-* RTI grants Licensee a license to use, modify, compile, and create derivative
-* works of the software solely for use with RTI Connext DDS. Licensee may
-* redistribute copies of the software provided that all such copies are subject
-* to this license. The software is provided "as is", with no warranty of any
-* type, including any warranty for fitness for any purpose. RTI is under no
-* obligation to maintain or support the software. RTI shall not be liable for
-* any incidental or consequential damages arising out of the use or inability
-* to use the software.
-*/
+ * (c) Copyright, Real-Time Innovations, 2021.  All rights reserved.
+ * RTI grants Licensee a license to use, modify, compile, and create derivative
+ * works of the software solely for use with RTI Connext DDS. Licensee may
+ * redistribute copies of the software provided that all such copies are subject
+ * to this license. The software is provided "as is", with no warranty of any
+ * type, including any warranty for fitness for any purpose. RTI is under no
+ * obligation to maintain or support the software. RTI shall not be liable for
+ * any incidental or consequential damages arising out of the use or inability
+ * to use the software.
+ */
 
 #include <algorithm>
 #include <iostream>
@@ -34,23 +34,26 @@ int process_data(dds::sub::DataReader<instance> reader)
     // Take all samples
     int count = 0;
     dds::sub::LoanedSamples<instance> samples = reader.take();
-    for (const auto& sample : samples) {
+    for (const auto &sample : samples) {
         if (sample.info().valid()) {
             count++;
             std::cout << sample.data() << std::endl;
         } else {
             std::cout << "Instance state changed to "
-            << sample.info().state().instance_state() << std::endl;
+                      << sample.info().state().instance_state() << std::endl;
         }
     }
 
-    return count; 
-} // The LoanedSamples destructor returns the loan
+    return count;
+}  // The LoanedSamples destructor returns the loan
 
-void run_subscriber_application(unsigned int domain_id, unsigned int sample_count)
+void run_subscriber_application(
+        unsigned int domain_id,
+        unsigned int sample_count)
 {
     // DDS objects behave like shared pointers or value types
-    // (see https://community.rti.com/static/documentation/connext-dds/6.1.1/doc/api/connext_dds/api_cpp2/group__DDSCpp2Conventions.html)
+    // (see
+    // https://community.rti.com/static/documentation/connext-dds/6.1.1/doc/api/connext_dds/api_cpp2/group__DDSCpp2Conventions.html)
 
     // Start communicating in a domain, usually one participant per application
     dds::domain::DomainParticipant participant(domain_id);
@@ -68,17 +71,16 @@ void run_subscriber_application(unsigned int domain_id, unsigned int sample_coun
     // Create a ReadCondition for any data on this reader, and add to WaitSet
     unsigned int samples_read = 0;
     dds::sub::cond::ReadCondition read_condition(
-        reader,
-        dds::sub::status::DataState::any(),
-        [reader, &samples_read]() {
-        samples_read += process_data(reader);
-    });
+            reader,
+            dds::sub::status::DataState::any(),
+            [reader, &samples_read]() {
+                samples_read += process_data(reader);
+            });
 
     waitset += read_condition;
 
     rti::core::status::DataReaderCacheStatus status;
     while (!application::shutdown_requested && samples_read < sample_count) {
-
         std::cout << "instance subscriber sleeping up to 1 sec..." << std::endl;
 
         // Wait for data and report if it does not arrive in 1 second
@@ -98,7 +100,6 @@ void run_subscriber_application(unsigned int domain_id, unsigned int sample_coun
 
 int main(int argc, char *argv[])
 {
-
     using namespace application;
 
     // Parse arguments and handle control-C
@@ -115,10 +116,10 @@ int main(int argc, char *argv[])
 
     try {
         run_subscriber_application(arguments.domain_id, arguments.sample_count);
-    } catch (const std::exception& ex) {
+    } catch (const std::exception &ex) {
         // This will catch DDS exceptions
         std::cerr << "Exception in run_subscriber_application(): " << ex.what()
-        << std::endl;
+                  << std::endl;
         return EXIT_FAILURE;
     }
 
