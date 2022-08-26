9493281eccd8784c95d38ff98a0cab4fdeda4b93
lulivi <luislivilla@gmail.com>
2022-08-26 19:06:57 +0200

    Remove build dir

--- examples/connext_dds/property_qos/c++98/buil/CMakeFiles/property_qos_cpp_qos.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/property_qos_cpp_qos"
-  "USER_QOS_PROFILES.xml"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/property_qos_cpp_qos.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
