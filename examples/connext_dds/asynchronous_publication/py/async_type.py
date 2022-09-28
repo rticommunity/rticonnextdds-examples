d66aed2ac26a38d29e1854b1427e3c362212ae5f
Anthony <Frenchy2199@yahoo.com>
2022-09-27 20:35:22 -0600

    Removing this file since it will be generated by rtiddsgen

--- examples/connext_dds/asynchronous_publication/py/async_type.py
+++
@@ -1,9 +0,0 @@
-import sys
-import rti.idl as idl
-from enum import IntEnum, auto
-from typing import Sequence, Optional
-from dataclasses import field
-
-@idl.struct
-class async_type:
-    x: idl.int32 = 0
