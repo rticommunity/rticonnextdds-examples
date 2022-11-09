9ee9412c0d6615ebaeedd972144476629a118045
lulivi <luislivilla@gmail.com>
2022-11-09 10:36:51 +0100

    Fix format issues

--- examples/connext_dds/build_systems/cmake/c++98/HelloWorld_publisher.cxx
+++ examples/connext_dds/build_systems/cmake/c++98/HelloWorld_publisher.cxx
@@ -1,14 +1,14 @@
 /*
-* (c) Copyright, Real-Time Innovations, 2020.  All rights reserved.
-* RTI grants Licensee a license to use, modify, compile, and create derivative
-* works of the software solely for use with RTI Connext DDS. Licensee may
-* redistribute copies of the software provided that all such copies are subject
-* to this license. The software is provided "as is", with no warranty of any
-* type, including any warranty for fitness for any purpose. RTI is under no
-* obligation to maintain or support the software. RTI shall not be liable for
-* any incidental or consequential damages arising out of the use or inability
-* to use the software.
-*/
+ * (c) Copyright, Real-Time Innovations, 2020.  All rights reserved.
+ * RTI grants Licensee a license to use, modify, compile, and create derivative
+ * works of the software solely for use with RTI Connext DDS. Licensee may
+ * redistribute copies of the software provided that all such copies are subject
+ * to this license. The software is provided "as is", with no warranty of any
+ * type, including any warranty for fitness for any purpose. RTI is under no
+ * obligation to maintain or support the software. RTI shall not be liable for
+ * any incidental or consequential damages arising out of the use or inability
+ * to use the software.
+ */
 
 #include <iostream>
 #include <stdio.h>
@@ -22,86 +22,103 @@
 using namespace application;
 
 static int shutdown_participant(
-    DDSDomainParticipant *participant,
-    const char *shutdown_message,
-    int status);
+        DDSDomainParticipant *participant,
+        const char *shutdown_message,
+        int status);
 
 int run_publisher_application(unsigned int domain_id, unsigned int sample_count)
 {
     // Start communicating in a domain, usually one participant per application
     DDSDomainParticipant *participant =
-    DDSTheParticipantFactory->create_participant(
-        domain_id,
-        DDS_PARTICIPANT_QOS_DEFAULT,
-        NULL /* listener */,
-        DDS_STATUS_MASK_NONE);
+            DDSTheParticipantFactory->create_participant(
+                    domain_id,
+                    DDS_PARTICIPANT_QOS_DEFAULT,
+                    NULL /* listener */,
+                    DDS_STATUS_MASK_NONE);
     if (participant == NULL) {
-        return shutdown_participant(participant, "create_participant error", EXIT_FAILURE);
+        return shutdown_participant(
+                participant,
+                "create_participant error",
+                EXIT_FAILURE);
     }
 
     // A Publisher allows an application to create one or more DataWriters
     DDSPublisher *publisher = participant->create_publisher(
-        DDS_PUBLISHER_QOS_DEFAULT,
-        NULL /* listener */,
-        DDS_STATUS_MASK_NONE);
+            DDS_PUBLISHER_QOS_DEFAULT,
+            NULL /* listener */,
+            DDS_STATUS_MASK_NONE);
     if (publisher == NULL) {
-        return shutdown_participant(participant, "create_publisher error", EXIT_FAILURE);
+        return shutdown_participant(
+                participant,
+                "create_publisher error",
+                EXIT_FAILURE);
     }
 
     // Register the datatype to use when creating the Topic
     const char *type_name = HelloWorldTypeSupport::get_type_name();
     DDS_ReturnCode_t retcode =
-    HelloWorldTypeSupport::register_type(participant, type_name);
+            HelloWorldTypeSupport::register_type(participant, type_name);
     if (retcode != DDS_RETCODE_OK) {
-        return shutdown_participant(participant, "register_type error", EXIT_FAILURE);
+        return shutdown_participant(
+                participant,
+                "register_type error",
+                EXIT_FAILURE);
     }
 
     // Create a Topic with a name and a datatype
     DDSTopic *topic = participant->create_topic(
-        "Example HelloWorld",
-        type_name,
-        DDS_TOPIC_QOS_DEFAULT,
-        NULL /* listener */,
-        DDS_STATUS_MASK_NONE);
+            "Example HelloWorld",
+            type_name,
+            DDS_TOPIC_QOS_DEFAULT,
+            NULL /* listener */,
+            DDS_STATUS_MASK_NONE);
     if (topic == NULL) {
-        return shutdown_participant(participant, "create_topic error", EXIT_FAILURE);
+        return shutdown_participant(
+                participant,
+                "create_topic error",
+                EXIT_FAILURE);
     }
 
     // This DataWriter writes data on "Example HelloWorld" Topic
     DDSDataWriter *untyped_writer = publisher->create_datawriter(
-        topic,
-        DDS_DATAWRITER_QOS_DEFAULT,
-        NULL /* listener */,
-        DDS_STATUS_MASK_NONE);
+            topic,
+            DDS_DATAWRITER_QOS_DEFAULT,
+            NULL /* listener */,
+            DDS_STATUS_MASK_NONE);
     if (untyped_writer == NULL) {
-        return shutdown_participant(participant, "create_datawriter error", EXIT_FAILURE);
+        return shutdown_participant(
+                participant,
+                "create_datawriter error",
+                EXIT_FAILURE);
     }
 
-    // Narrow casts from an untyped DataWriter to a writer of your type 
+    // Narrow casts from an untyped DataWriter to a writer of your type
     HelloWorldDataWriter *typed_writer =
-    HelloWorldDataWriter::narrow(untyped_writer);
+            HelloWorldDataWriter::narrow(untyped_writer);
     if (typed_writer == NULL) {
-        return shutdown_participant(participant, "DataWriter narrow error", EXIT_FAILURE);
+        return shutdown_participant(
+                participant,
+                "DataWriter narrow error",
+                EXIT_FAILURE);
     }
 
     // Create data for writing, allocating all members
     HelloWorld *data = HelloWorldTypeSupport::create_data();
     if (data == NULL) {
         return shutdown_participant(
-            participant,
-            "HelloWorldTypeSupport::create_data error",
-            EXIT_FAILURE);
+                participant,
+                "HelloWorldTypeSupport::create_data error",
+                EXIT_FAILURE);
     }
 
     // Main loop, write data
     for (unsigned int samples_written = 0;
-    !shutdown_requested && samples_written < sample_count;
-    ++samples_written) {
-
+         !shutdown_requested && samples_written < sample_count;
+         ++samples_written) {
         // Modify the data to be written here
 
-        std::cout << "Writing HelloWorld, count " << samples_written 
-        << std::endl;
+        std::cout << "Writing HelloWorld, count " << samples_written
+                  << std::endl;
         retcode = typed_writer->write(*data, DDS_HANDLE_NIL);
         if (retcode != DDS_RETCODE_OK) {
             std::cerr << "write error " << retcode << std::endl;
@@ -116,7 +133,7 @@ int run_publisher_application(unsigned int domain_id, unsigned int sample_count)
     retcode = HelloWorldTypeSupport::delete_data(data);
     if (retcode != DDS_RETCODE_OK) {
         std::cerr << "HelloWorldTypeSupport::delete_data error " << retcode
-        << std::endl;
+                  << std::endl;
     }
 
     // Delete all entities (DataWriter, Topic, Publisher, DomainParticipant)
@@ -125,9 +142,9 @@ int run_publisher_application(unsigned int domain_id, unsigned int sample_count)
 
 // Delete all entities
 static int shutdown_participant(
-    DDSDomainParticipant *participant,
-    const char *shutdown_message,
-    int status)
+        DDSDomainParticipant *participant,
+        const char *shutdown_message,
+        int status)
 {
     DDS_ReturnCode_t retcode;
 
@@ -138,7 +155,7 @@ static int shutdown_participant(
         retcode = participant->delete_contained_entities();
         if (retcode != DDS_RETCODE_OK) {
             std::cerr << "delete_contained_entities error " << retcode
-            << std::endl;
+                      << std::endl;
             status = EXIT_FAILURE;
         }
 
@@ -154,7 +171,6 @@ static int shutdown_participant(
 
 int main(int argc, char *argv[])
 {
-
     // Parse arguments and handle control-C
     ApplicationArguments arguments;
     parse_arguments(arguments, argc, argv);
@@ -168,7 +184,9 @@ int main(int argc, char *argv[])
     // Sets Connext verbosity to help debugging
     NDDSConfigLogger::get_instance()->set_verbosity(arguments.verbosity);
 
-    int status = run_publisher_application(arguments.domain_id, arguments.sample_count);
+    int status = run_publisher_application(
+            arguments.domain_id,
+            arguments.sample_count);
 
     // Releases the memory used by the participant factory.  Optional at
     // application exit
@@ -180,4 +198,3 @@ int main(int argc, char *argv[])
 
     return status;
 }
-
--- examples/connext_dds/build_systems/cmake/c++98/HelloWorld_subscriber.cxx
+++ examples/connext_dds/build_systems/cmake/c++98/HelloWorld_subscriber.cxx
@@ -1,14 +1,14 @@
 /*
-* (c) Copyright, Real-Time Innovations, 2020.  All rights reserved.
-* RTI grants Licensee a license to use, modify, compile, and create derivative
-* works of the software solely for use with RTI Connext DDS. Licensee may
-* redistribute copies of the software provided that all such copies are subject
-* to this license. The software is provided "as is", with no warranty of any
-* type, including any warranty for fitness for any purpose. RTI is under no
-* obligation to maintain or support the software. RTI shall not be liable for
-* any incidental or consequential damages arising out of the use or inability
-* to use the software.
-*/
+ * (c) Copyright, Real-Time Innovations, 2020.  All rights reserved.
+ * RTI grants Licensee a license to use, modify, compile, and create derivative
+ * works of the software solely for use with RTI Connext DDS. Licensee may
+ * redistribute copies of the software provided that all such copies are subject
+ * to this license. The software is provided "as is", with no warranty of any
+ * type, including any warranty for fitness for any purpose. RTI is under no
+ * obligation to maintain or support the software. RTI shall not be liable for
+ * any incidental or consequential damages arising out of the use or inability
+ * to use the software.
+ */
 
 #include <iostream>
 #include <stdio.h>
@@ -22,25 +22,25 @@
 using namespace application;
 
 static int shutdown_participant(
-    DDSDomainParticipant *participant,
-    const char *shutdown_message,
-    int status);
+        DDSDomainParticipant *participant,
+        const char *shutdown_message,
+        int status);
 
 // Process data. Returns number of samples processed.
 unsigned int process_data(HelloWorldDataReader *typed_reader)
 {
-    HelloWorldSeq data_seq;     // Sequence of received data
-    DDS_SampleInfoSeq info_seq; // Metadata associated with samples in data_seq
+    HelloWorldSeq data_seq;      // Sequence of received data
+    DDS_SampleInfoSeq info_seq;  // Metadata associated with samples in data_seq
     unsigned int samples_read = 0;
 
     // Take available data from DataReader's queue
     typed_reader->take(
-        data_seq,
-        info_seq,
-        DDS_LENGTH_UNLIMITED,
-        DDS_ANY_SAMPLE_STATE,
-        DDS_ANY_VIEW_STATE,
-        DDS_ANY_INSTANCE_STATE);
+            data_seq,
+            info_seq,
+            DDS_LENGTH_UNLIMITED,
+            DDS_ANY_SAMPLE_STATE,
+            DDS_ANY_VIEW_STATE,
+            DDS_ANY_INSTANCE_STATE);
 
     // Iterate over all available data
     for (int i = 0; i < data_seq.length(); ++i) {
@@ -63,78 +63,104 @@ unsigned int process_data(HelloWorldDataReader *typed_reader)
     return samples_read;
 }
 
-int run_subscriber_application(unsigned int domain_id, unsigned int sample_count)
+int run_subscriber_application(
+        unsigned int domain_id,
+        unsigned int sample_count)
 {
     // Start communicating in a domain, usually one participant per application
     DDSDomainParticipant *participant =
-    DDSTheParticipantFactory->create_participant(
-        domain_id,
-        DDS_PARTICIPANT_QOS_DEFAULT,
-        NULL /* listener */,
-        DDS_STATUS_MASK_NONE);
+            DDSTheParticipantFactory->create_participant(
+                    domain_id,
+                    DDS_PARTICIPANT_QOS_DEFAULT,
+                    NULL /* listener */,
+                    DDS_STATUS_MASK_NONE);
     if (participant == NULL) {
-        return shutdown_participant(participant, "create_participant error", EXIT_FAILURE);
+        return shutdown_participant(
+                participant,
+                "create_participant error",
+                EXIT_FAILURE);
     }
 
     // A Subscriber allows an application to create one or more DataReaders
     DDSSubscriber *subscriber = participant->create_subscriber(
-        DDS_SUBSCRIBER_QOS_DEFAULT,
-        NULL /* listener */,
-        DDS_STATUS_MASK_NONE);
+            DDS_SUBSCRIBER_QOS_DEFAULT,
+            NULL /* listener */,
+            DDS_STATUS_MASK_NONE);
     if (subscriber == NULL) {
-        return shutdown_participant(participant, "create_subscriber error", EXIT_FAILURE);
+        return shutdown_participant(
+                participant,
+                "create_subscriber error",
+                EXIT_FAILURE);
     }
 
     // Register the datatype to use when creating the Topic
     const char *type_name = HelloWorldTypeSupport::get_type_name();
     DDS_ReturnCode_t retcode =
-    HelloWorldTypeSupport::register_type(participant, type_name);
+            HelloWorldTypeSupport::register_type(participant, type_name);
     if (retcode != DDS_RETCODE_OK) {
-        return shutdown_participant(participant, "register_type error", EXIT_FAILURE);
+        return shutdown_participant(
+                participant,
+                "register_type error",
+                EXIT_FAILURE);
     }
 
     // Create a Topic with a name and a datatype
     DDSTopic *topic = participant->create_topic(
-        "Example HelloWorld",
-        type_name,
-        DDS_TOPIC_QOS_DEFAULT,
-        NULL /* listener */,
-        DDS_STATUS_MASK_NONE);
+            "Example HelloWorld",
+            type_name,
+            DDS_TOPIC_QOS_DEFAULT,
+            NULL /* listener */,
+            DDS_STATUS_MASK_NONE);
     if (topic == NULL) {
-        return shutdown_participant(participant, "create_topic error", EXIT_FAILURE);
+        return shutdown_participant(
+                participant,
+                "create_topic error",
+                EXIT_FAILURE);
     }
 
     // This DataReader reads data on "Example HelloWorld" Topic
     DDSDataReader *untyped_reader = subscriber->create_datareader(
-        topic,
-        DDS_DATAREADER_QOS_DEFAULT,
-        NULL,
-        DDS_STATUS_MASK_NONE);
+            topic,
+            DDS_DATAREADER_QOS_DEFAULT,
+            NULL,
+            DDS_STATUS_MASK_NONE);
     if (untyped_reader == NULL) {
-        return shutdown_participant(participant, "create_datareader error", EXIT_FAILURE);
+        return shutdown_participant(
+                participant,
+                "create_datareader error",
+                EXIT_FAILURE);
     }
 
     // Narrow casts from a untyped DataReader to a reader of your type
     HelloWorldDataReader *typed_reader =
-    HelloWorldDataReader::narrow(untyped_reader);
+            HelloWorldDataReader::narrow(untyped_reader);
     if (typed_reader == NULL) {
-        return shutdown_participant(participant, "DataReader narrow error", EXIT_FAILURE);
+        return shutdown_participant(
+                participant,
+                "DataReader narrow error",
+                EXIT_FAILURE);
     }
 
     // Create ReadCondition that triggers when unread data in reader's queue
     DDSReadCondition *read_condition = typed_reader->create_readcondition(
-        DDS_NOT_READ_SAMPLE_STATE,
-        DDS_ANY_VIEW_STATE,
-        DDS_ANY_INSTANCE_STATE);
+            DDS_NOT_READ_SAMPLE_STATE,
+            DDS_ANY_VIEW_STATE,
+            DDS_ANY_INSTANCE_STATE);
     if (read_condition == NULL) {
-        return shutdown_participant(participant, "create_readcondition error", EXIT_FAILURE);
+        return shutdown_participant(
+                participant,
+                "create_readcondition error",
+                EXIT_FAILURE);
     }
 
     // WaitSet will be woken when the attached condition is triggered
     DDSWaitSet waitset;
     retcode = waitset.attach_condition(read_condition);
     if (retcode != DDS_RETCODE_OK) {
-        return shutdown_participant(participant, "attach_condition error", EXIT_FAILURE);
+        return shutdown_participant(
+                participant,
+                "attach_condition error",
+                EXIT_FAILURE);
     }
 
     // Main loop. Wait for data to arrive, and process when it arrives
@@ -162,9 +188,9 @@ int run_subscriber_application(unsigned int domain_id, unsigned int sample_count
 
 // Delete all entities
 static int shutdown_participant(
-    DDSDomainParticipant *participant,
-    const char *shutdown_message,
-    int status)
+        DDSDomainParticipant *participant,
+        const char *shutdown_message,
+        int status)
 {
     DDS_ReturnCode_t retcode;
 
@@ -175,7 +201,7 @@ static int shutdown_participant(
         retcode = participant->delete_contained_entities();
         if (retcode != DDS_RETCODE_OK) {
             std::cerr << "delete_contained_entities error" << retcode
-            << std::endl;
+                      << std::endl;
             status = EXIT_FAILURE;
         }
 
@@ -190,7 +216,6 @@ static int shutdown_participant(
 
 int main(int argc, char *argv[])
 {
-
     // Parse arguments and handle control-C
     ApplicationArguments arguments;
     parse_arguments(arguments, argc, argv);
@@ -204,7 +229,9 @@ int main(int argc, char *argv[])
     // Sets Connext verbosity to help debugging
     NDDSConfigLogger::get_instance()->set_verbosity(arguments.verbosity);
 
-    int status = run_subscriber_application(arguments.domain_id, arguments.sample_count);
+    int status = run_subscriber_application(
+            arguments.domain_id,
+            arguments.sample_count);
 
     // Releases the memory used by the participant factory.  Optional at
     // application exit
