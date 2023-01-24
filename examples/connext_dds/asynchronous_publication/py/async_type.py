fe847beb84a8fc9d4bca55703af1241aa9daeaea
Anthony Dupont <Frenchy2199@yahoo.com>
2023-01-24 16:57:15 -0700

    Adding type file to skip code generation step

---
+++ examples/connext_dds/asynchronous_publication/py/async_type.py
@@ -0,0 +1,18 @@
+# WARNING: THIS FILE IS AUTO-GENERATED. DO NOT MODIFY.
+
+# This file was generated from async.idl
+# using RTI Code Generator (rtiddsgen) version 3.1.1.
+# The rtiddsgen tool is part of the RTI Connext DDS distribution.
+# For more information, type 'rtiddsgen -help' at a command shell
+# or consult the Code Generator User's Manual.
+
+import sys
+import rti.idl as idl
+from enum import IntEnum, auto
+from typing import Sequence, Optional
+from dataclasses import field
+
+
+@idl.struct
+class async_type:
+    x: idl.int32 = 0
