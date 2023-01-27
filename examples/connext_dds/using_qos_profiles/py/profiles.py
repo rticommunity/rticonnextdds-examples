caebc12824ea3a1a2e32582cefaa69dc0d0e9275
Alex Campos <alejandro@rti.com>
2023-01-27 13:48:30 -0800

    Add profiles/py example

---
+++ examples/connext_dds/using_qos_profiles/py/profiles.py
@@ -0,0 +1,21 @@
+# (c) Copyright, Real-Time Innovations, 2023.  All rights reserved.
+# RTI grants Licensee a license to use, modify, compile, and create derivative
+# works of the software solely for use with RTI Connext DDS. Licensee may
+# redistribute copies of the software provided that all such copies are subject
+# to this license. The software is provided "as is", with no warranty of any
+# type, including any warranty for fitness for any purpose. RTI is under no
+# obligation to maintain or support the software. RTI shall not be liable for
+# any incidental or consequential damages arising out of the use or inability
+# to use the software.
+
+import rti.idl as idl
+
+
+@idl.struct(
+    member_annotations={
+        "profile_name": [idl.key, idl.bound(100)],
+    }
+)
+class ProfilesExample:
+    profile_name: str = ""
+    x: idl.int32 = 0
