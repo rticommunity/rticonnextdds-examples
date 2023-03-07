969f4348e24a804be8e8e0c66ecad3b203cdd590
Anthony <38296555+xFrenchy@users.noreply.github.com>
2023-03-07 11:48:30 -0700

    Python async pub example (#537)
    * Initial commit, adding async pub example
    
    * Renaming file due to Python async keyword
    
    * Adding first version of example
    
    * Removing this file since it will be generated by rtiddsgen
    
    * Adding programatically setting QoS in publisher/subscriber, not fully working yet
    
    * Updating comments
    
    * Added QoS options and changed to printf
    
    * Added asyncio and QoS options
    
    * Consistency in QoS calls, removing unneeded calls and comments
    
    * Adding type file to skip code generation step
    
    * Removing some comments and unneeded calls. Properly calling the async publish function as well
    
    * Adding max wait time for async publishing
    
    * Replaced header
    
    * Addressed the commented notes
    
    * Adding readme

---
+++ examples/connext_dds/asynchronous_publication/py/async_publisher.py
@@ -0,0 +1,130 @@
+#
+# (c) 2022 Copyright, Real-Time Innovations, Inc.  All rights reserved.
+#
+# RTI grants Licensee a license to use, modify, compile, and create derivative
+# works of the Software solely for use with RTI products.  The Software is
+# provided "as is", with no warranty of any type, including any warranty for
+# fitness for any purpose. RTI is under no obligation to maintain or support
+# the Software.  RTI shall not be liable for any incidental or consequential
+# damages arising out of the use or inability to use the software.
+#
+import argparse
+import time
+
+import rti.connextdds as dds
+
+from async_type import async_type
+
+
+def run_publisher_application(
+    domain_id: int, sample_count: int, use_xml_qos: bool = False
+):
+    # Start communicating in a domain, usually one participant per application
+    participant = dds.DomainParticipant(domain_id)
+
+    # Create a Topic
+    topic = dds.Topic(participant, "Example async", async_type)
+
+    if use_xml_qos:
+        # Retrieve the default DataWriter QoS, from USER_QOS_PROFILES.xml
+        writer_qos = dds.QosProvider.default.datawriter_qos
+    else:
+        # Configure the DataWriterQos in code, to the same effect as the XML file.
+        writer_qos = participant.default_datawriter_qos
+        writer_qos.reliability = dds.Reliability.reliable(
+            dds.Duration.from_seconds(60)
+        )
+        writer_qos.history.kind = dds.HistoryKind.KEEP_LAST
+        writer_qos.history.depth = 12
+        writer_qos.data_writer_protocol.rtps_reliable_writer.min_send_window_size = (
+            50
+        )
+        writer_qos.data_writer_protocol.rtps_reliable_writer.max_send_window_size = (
+            50
+        )
+
+        writer_qos.publish_mode = dds.PublishMode.asynchronous(
+            "DDS_FIXED_RATE_FLOW_CONTROLLER_NAME"
+        )
+
+    # Create a DataWriter with the QoS in our profile or configured programatically
+    writer = dds.DataWriter(participant.implicit_publisher, topic, writer_qos)
+
+    # Create data sample for writing
+    sample = async_type(x=42)
+
+    for count in range(sample_count):
+        try:
+            # Modify the data to be sent here
+            sample.x = count
+            print(f"Writing async_type, count {count}")
+            writer.write(sample)
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
+    # You can wait until all written samples have been actually published
+    # (note that this doesn't ensure that they have actually been received
+    # if the sample required retransmission)
+    writer.wait_for_asynchronous_publishing(dds.Duration(10))
+    print("preparing to shut down...")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="RTI Connext DDS Example: Using Asynchronous Publishing"
+    )
+    parser.add_argument(
+        "-d",
+        "--domain",
+        type=int,
+        default=0,
+        help="DDS Domain ID | Range: 0-232 | Default: 0",
+    )
+    parser.add_argument(
+        "-c",
+        "--count",
+        type=int,
+        default=50,
+        help="Number of samples to send | Default 50",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbosity",
+        type=int,
+        default=1,
+        help="How much debugging output to show | Range: 0-3 | Default: 1",
+    )
+    parser.add_argument(
+        "-q",
+        "--qos",
+        action="store_true",
+        default=False,
+        help="Use QoS profile from USER_QOS_PROFILES.xml | Default: False",
+    )
+
+    args = parser.parse_args()
+    assert 0 <= args.domain < 233
+    assert args.count >= 0
+    assert 0 <= args.verbosity < 4
+
+    verbosity_levels = {
+        0: dds.Verbosity.SILENT,
+        1: dds.Verbosity.EXCEPTION,
+        2: dds.Verbosity.WARNING,
+        3: dds.Verbosity.STATUS_ALL,
+    }
+
+    # Sets Connext verbosity to help debugging
+    verbosity = verbosity_levels.get(args.verbosity, dds.Verbosity.EXCEPTION)
+
+    dds.Logger.instance.verbosity = verbosity
+
+    try:
+        run_publisher_application(args.domain, args.count, args.qos)
+    except Exception as e:
+        print(f"Exception in run_publisher_application(): {e}")
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/connext_dds/asynchronous_publication/py/async_subscriber.py
@@ -0,0 +1,114 @@
+#
+# (c) 2022 Copyright, Real-Time Innovations, Inc.  All rights reserved.
+#
+# RTI grants Licensee a license to use, modify, compile, and create derivative
+# works of the Software solely for use with RTI products.  The Software is
+# provided "as is", with no warranty of any type, including any warranty for
+# fitness for any purpose. RTI is under no obligation to maintain or support
+# the Software.  RTI shall not be liable for any incidental or consequential
+# damages arising out of the use or inability to use the software.
+#
+import argparse
+import rti.asyncio
+
+import rti.connextdds as dds
+
+from async_type import async_type
+
+
+async def process_data(reader: dds.DataReader):
+    # Print data as it arrives, suspending the coroutine until data is
+    # available.
+    async for data in reader.take_data_async():
+        print(f"Received: {data}")
+
+
+def run_subscriber_application(
+    domain_id: int, sample_count: int, use_xml_qos: bool = False
+):
+    # Start communicating in a domain, usually one participant per application
+    participant = dds.DomainParticipant(domain_id)
+
+    # Create a Topic
+    topic = dds.Topic(participant, "Example async", async_type)
+
+    if use_xml_qos:
+        # Retrieve the default DataWriter QoS, from USER_QOS_PROFILES.xml
+        reader_qos = dds.QosProvider.default.datareader_qos
+    else:
+        # Configure the DataReaderQos in code, to the same effect as the XML file.
+        reader_qos = participant.default_datareader_qos
+        reader_qos.reliability.kind = dds.ReliabilityKind.RELIABLE
+        reader_qos.history.kind = dds.HistoryKind.KEEP_ALL
+
+    # Create a DataReader with the QoS in our profile or configured programatically
+    reader = dds.DataReader(participant.implicit_subscriber, topic, reader_qos)
+
+    try:
+        print("async subscriber sleeping")
+        rti.asyncio.run(process_data(reader))
+    except KeyboardInterrupt:
+        pass
+
+    print("preparing to shut down...")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="RTI Connext DDS Example: Using Asynchronous Publishing"
+    )
+    parser.add_argument(
+        "-d",
+        "--domain",
+        type=int,
+        default=0,
+        help="DDS Domain ID | Range: 0-232 | Default: 0",
+    )
+    parser.add_argument(
+        "-c",
+        "--count",
+        type=int,
+        default=50,
+        help="Number of samples to send | Default 50",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbosity",
+        type=int,
+        default=1,
+        help="How much debugging output to show | Range: 0-3 | Default: 1",
+    )
+    parser.add_argument(
+        "-q",
+        "--qos",
+        action="store_true",
+        default=False,
+        help="Use QoS profile from USER_QOS_PROFILES.xml | Default: False",
+    )
+
+    args = parser.parse_args()
+    assert 0 <= args.domain < 233
+    assert args.count >= 0
+    assert 0 <= args.verbosity < 4
+
+    verbosity_levels = {
+        0: dds.Verbosity.SILENT,
+        1: dds.Verbosity.EXCEPTION,
+        2: dds.Verbosity.WARNING,
+        3: dds.Verbosity.STATUS_ALL,
+    }
+
+    # Sets Connext verbosity to help debugging
+    verbosity = verbosity_levels.get(args.verbosity, dds.Verbosity.EXCEPTION)
+
+    dds.Logger.instance.verbosity = verbosity
+
+    try:
+        run_subscriber_application(args.domain, args.count, args.qos)
+    except Exception as e:
+        print(f"Exception in run_subscriber_application(): {e}")
+    return
+
+
+if __name__ == "__main__":
+    main()
