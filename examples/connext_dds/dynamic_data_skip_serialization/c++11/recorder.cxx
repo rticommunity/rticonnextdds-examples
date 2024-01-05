c1a64039c0c8aeb33a138b66eb2f16ac01c94839
Alex Campos <alejandro@rti.com>
2024-01-05 10:32:04 -0800

    Add dynamic_data_skip_serialization example (#642)
    Add dynamic_data_skip_serialization example for C++11 and Python. This is an upcoming feature in Connext DDS 7.3.0.

---
+++ examples/connext_dds/dynamic_data_skip_serialization/c++11/recorder.cxx
@@ -0,0 +1,195 @@
+/*
+ * (c) Copyright, Real-Time Innovations, 2023.  All rights reserved.
+ * RTI grants Licensee a license to use, modify, compile, and create derivative
+ * works of the software solely for use with RTI Connext DDS. Licensee may
+ * redistribute copies of the software provided that all such copies are subject
+ * to this license. The software is provided "as is", with no warranty of any
+ * type, including any warranty for fitness for any purpose. RTI is under no
+ * obligation to maintain or support the software. RTI shall not be liable for
+ * any incidental or consequential damages arising out of the use or inability
+ * to use the software.
+ */
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <rti/rti.hpp>
+
+#include "util.hpp"
+
+dds::core::xtypes::StructType create_type()
+{
+    using namespace dds::core::xtypes;
+
+    StructType type(
+            "RecordExample",
+            { Member("id", StringType(128)),
+              Member("payload",
+                     SequenceType(primitive_type<int32_t>(), 1024)) });
+
+    return type;
+}
+
+void record(const std::string &file_name, int domain_id)
+{
+    using dds::core::xtypes::DynamicData;
+
+    // To disable the deserialization on the DataReader and get direct access
+    // to the serialized CDR buffer, we need to register the DataReader type
+    // with the DynamicDataTypeSerializationProperty::skip_deserialization set
+    // to true, and then create the DataReader with the name used to register
+    // the type.
+    dds::domain::DomainParticipant participant(domain_id);
+    const std::string type_name = "RecordExample";
+    rti::core::xtypes::DynamicDataTypeSerializationProperty property;
+    property.skip_deserialization(true);
+    rti::domain::register_type(participant, type_name, create_type(), property);
+
+    dds::topic::Topic<DynamicData> topic(
+            participant,
+            "Example Record",
+            type_name);  // specify the registered type name
+
+    auto qos = dds::core::QosProvider::Default().datareader_qos(
+            rti::core::builtin_profiles::qos_lib::generic_strict_reliable());
+    dds::sub::DataReader<DynamicData> reader(topic, qos);
+    if (!util::wait_for_writer(reader)) {
+        return;
+    }
+
+    // File setup
+    std::ofstream out_file(file_name, std::ios::binary);
+    if (!out_file) {
+        std::cerr << "Failed to open file for recording: " << file_name
+                  << std::endl;
+        return;
+    }
+
+    auto record_data = [&out_file, &reader]() {
+        auto samples = reader.take();
+        for (auto sample : samples) {
+            if (!sample.info().valid()) {
+                continue;
+            }
+
+            // Now the only way to access the data is to call get_cdr_buffer,
+            // any other field accessor will fail.
+            auto buffer_info = sample.data().get_cdr_buffer();
+            auto buffer = buffer_info.first;
+            auto buffer_length = buffer_info.second;
+            std::cout << "Recording data sample (" << buffer_length << " bytes)"
+                      << std::endl;
+            out_file.write(
+                    reinterpret_cast<const char *>(&buffer_length),
+                    sizeof(buffer_length));
+            out_file.write(
+                    reinterpret_cast<const char *>(buffer),
+                    buffer_length);
+        }
+    };
+
+    // Set up a ReadCondition to trigger the record_data function when
+    // data is available
+    dds::core::cond::WaitSet waitset;
+    dds::sub::cond::ReadCondition read_condition(
+            reader,
+            dds::sub::status::DataState::any(),
+            record_data);
+    waitset += read_condition;
+
+    while (!application::shutdown_requested) {
+        waitset.dispatch(dds::core::Duration(1));
+    }
+}
+
+void replay(const std::string &file_name, int domain_id)
+{
+    using dds::core::xtypes::DynamicData;
+
+    dds::domain::DomainParticipant participant(domain_id);
+
+    // For the replay application we don't need to register the type with any
+    // particular property because DynamicData DataWriters are always prepared
+    // to write serialized buffers directly
+    dds::topic::Topic<DynamicData> topic(
+            participant,
+            "Example Record",
+            create_type());
+
+    auto qos = dds::core::QosProvider::Default().datawriter_qos(
+            rti::core::builtin_profiles::qos_lib::generic_strict_reliable());
+    dds::pub::DataWriter<DynamicData> writer(topic, qos);
+    if (!util::wait_for_reader(writer)) {
+        return;
+    }
+
+    std::ifstream in_file(file_name, std::ios::binary);
+    if (!in_file) {
+        std::cerr << "Failed to open file for replay: " << file_name
+                  << std::endl;
+        return;
+    }
+
+    uint32_t length;
+    std::vector<char> buffer;
+    DynamicData sample(create_type());
+    while (!in_file.eof()) {
+        // read length and data
+        if (!in_file.read(reinterpret_cast<char *>(&length), sizeof(length))) {
+            break;
+        }
+
+        std::cout << "Replaying data sample (" << length << " bytes)"
+                  << std::endl;
+        buffer.resize(length);
+        if (!in_file.read(buffer.data(), length)) {
+            break;
+        }
+
+        // By calling the set_cdr_buffer method we override the contents
+        // of the DynamicData object with the new serialized data. After
+        // setting a cdr buffer we can't use any field getters or setters.
+        sample.set_cdr_buffer(buffer.data(), length);
+        writer.write(sample);
+    }
+
+    in_file.close();
+    writer.wait_for_acknowledgments(dds::core::Duration(10));
+}
+
+int main(int argc, char *argv[])
+{
+    using namespace application;
+
+    // Parse arguments and handle control-C
+    auto arguments = parse_arguments(argc, argv);
+    if (arguments.parse_result == ParseReturn::exit) {
+        return EXIT_SUCCESS;
+    } else if (arguments.parse_result == ParseReturn::failure) {
+        return EXIT_FAILURE;
+    }
+    setup_signal_handlers();
+
+    // Sets Connext verbosity to help debugging
+    rti::config::Logger::instance().verbosity(arguments.verbosity);
+
+    try {
+        if (arguments.application_type == ApplicationType::record) {
+            record(arguments.file_name, arguments.domain_id);
+        } else if (arguments.application_type == ApplicationType::replay) {
+            replay(arguments.file_name, arguments.domain_id);
+        } else if (arguments.application_type == ApplicationType::publish) {
+            util::publish_example_data(arguments.domain_id, create_type());
+        }
+
+    } catch (const std::exception &ex) {
+        std::cerr << "Exception in application: " << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Releases the memory used by the participant factory.  Optional at
+    // application exit
+    dds::domain::DomainParticipant::finalize_participant_factory();
+
+    return EXIT_SUCCESS;
+}
---
+++ examples/connext_dds/dynamic_data_skip_serialization/c++11/util.cxx
@@ -0,0 +1,175 @@
+/*
+ * (c) Copyright, Real-Time Innovations, 2023.  All rights reserved.
+ * RTI grants Licensee a license to use, modify, compile, and create derivative
+ * works of the software solely for use with RTI Connext DDS. Licensee may
+ * redistribute copies of the software provided that all such copies are subject
+ * to this license. The software is provided "as is", with no warranty of any
+ * type, including any warranty for fitness for any purpose. RTI is under no
+ * obligation to maintain or support the software. RTI shall not be liable for
+ * any incidental or consequential damages arising out of the use or inability
+ * to use the software.
+ */
+
+#include "util.hpp"
+
+#include <numeric>
+
+namespace util {
+
+void publish_example_data(
+        unsigned int domain_id,
+        const dds::core::xtypes::DynamicType &type)
+{
+    dds::domain::DomainParticipant participant(domain_id);
+    dds::topic::Topic<dds::core::xtypes::DynamicData> topic(
+            participant,
+            "Example Record",
+            type);
+
+    auto qos = dds::core::QosProvider::Default().datawriter_qos(
+            rti::core::builtin_profiles::qos_lib::generic_strict_reliable());
+    dds::pub::DataWriter<dds::core::xtypes::DynamicData> writer(topic, qos);
+    if (!wait_for_reader(writer)) {
+        return;
+    }
+
+    // Write a few data samples
+    dds::core::xtypes::DynamicData data(type);
+    rti::util::sleep(dds::core::Duration::from_secs(1));
+    data.value("id", std::string("Example"));
+    std::vector<int32_t> payload(10);
+    std::iota(payload.begin(), payload.end(), 0);
+    data.set_values("payload", payload);
+    writer.write(data);
+
+    rti::util::sleep(dds::core::Duration::from_secs(1));
+    data.value("id", std::string("Example2"));
+    payload.resize(5);
+    data.set_values("payload", payload);
+    writer.write(data);
+
+    rti::util::sleep(dds::core::Duration::from_secs(1));
+    data.value("id", std::string("Example3"));
+    payload.resize(30);
+    std::iota(payload.begin(), payload.end(), 100);
+    data.set_values("payload", payload);
+    writer.write(data);
+
+    writer.wait_for_acknowledgments(dds::core::Duration(5));
+}
+
+}  // namespace util
+
+namespace application {
+bool shutdown_requested = false;
+
+void set_verbosity(rti::config::Verbosity &verbosity, int verbosity_value)
+{
+    switch (verbosity_value) {
+    case 0:
+        verbosity = rti::config::Verbosity::SILENT;
+        break;
+    case 1:
+        verbosity = rti::config::Verbosity::EXCEPTION;
+        break;
+    case 2:
+        verbosity = rti::config::Verbosity::WARNING;
+        break;
+    case 3:
+        verbosity = rti::config::Verbosity::STATUS_ALL;
+        break;
+    default:
+        verbosity = rti::config::Verbosity::EXCEPTION;
+        break;
+    }
+}
+
+ApplicationArguments parse_arguments(int argc, char *argv[])
+{
+    int arg_processing = 1;
+    bool show_usage = false;
+    ParseReturn parse_result = ParseReturn::ok;
+    unsigned int domain_id = 0;
+    unsigned int sample_count = (std::numeric_limits<unsigned int>::max)();
+    ApplicationType app_type = ApplicationType::unknown;
+    std::string file_name;
+    rti::config::Verbosity verbosity(rti::config::Verbosity::EXCEPTION);
+
+    while (arg_processing < argc) {
+        if ((argc > arg_processing + 1)
+            && (strcmp(argv[arg_processing], "-d") == 0
+                || strcmp(argv[arg_processing], "--domain") == 0)) {
+            domain_id = atoi(argv[arg_processing + 1]);
+            arg_processing += 2;
+        } else if (
+                (argc > arg_processing + 1)
+                && (strcmp(argv[arg_processing], "-s") == 0
+                    || strcmp(argv[arg_processing], "--sample-count") == 0)) {
+            sample_count = atoi(argv[arg_processing + 1]);
+            arg_processing += 2;
+        } else if (
+                (argc > arg_processing + 1)
+                && (strcmp(argv[arg_processing], "-v") == 0
+                    || strcmp(argv[arg_processing], "--verbosity") == 0)) {
+            set_verbosity(verbosity, atoi(argv[arg_processing + 1]));
+            arg_processing += 2;
+            // parse file_name
+        } else if (
+                (argc > arg_processing + 1)
+                && (strcmp(argv[arg_processing], "--record") == 0)) {
+            file_name = argv[arg_processing + 1];
+            app_type = ApplicationType::record;
+            arg_processing += 2;
+        } else if (
+                (argc > arg_processing + 1)
+                && (strcmp(argv[arg_processing], "--replay") == 0)) {
+            file_name = argv[arg_processing + 1];
+            app_type = ApplicationType::replay;
+            arg_processing += 2;
+        } else if (strcmp(argv[arg_processing], "--publish") == 0) {
+            app_type = ApplicationType::publish;
+            arg_processing += 1;
+        } else if (
+                strcmp(argv[arg_processing], "-h") == 0
+                || strcmp(argv[arg_processing], "--help") == 0) {
+            std::cout << "Example application." << std::endl;
+            show_usage = true;
+            parse_result = ParseReturn::exit;
+            break;
+        } else {
+            std::cout << "Bad parameter." << std::endl;
+            show_usage = true;
+            parse_result = ParseReturn::failure;
+            break;
+        }
+    }
+
+    if (app_type == ApplicationType::unknown) {
+        std::cout << "Must specify --record, --replay, or --publish."
+                  << std::endl;
+        show_usage = true;
+        parse_result = ParseReturn::failure;
+    }
+
+    if (show_usage) {
+        std::cout << "Options:\n"
+                     "    --record <file name>       Record to a file.\n"
+                     "    --replay <file name>       Replay from a file.\n"
+                     "    --publish                  Publish example data.\n"
+                     "    -d, --domain <int>         Domain ID this "
+                     "application will\n"
+                     "                               publish or subscribe "
+                     "in.  \n"
+                     "                               Default: 0\n"
+                  << std::endl;
+    }
+
+    return ApplicationArguments(
+            parse_result,
+            domain_id,
+            sample_count,
+            app_type,
+            file_name,
+            verbosity);
+}
+}  // namespace application
