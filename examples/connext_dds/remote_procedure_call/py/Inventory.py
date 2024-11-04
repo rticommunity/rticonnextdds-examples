7d07104d4884d490f8e0bce027cfee6c70153e7b
Alberto Robles <alberto.robles.hernandez@gmail.com>
2024-11-04 17:03:10 +0100

    REQREPLY-221: Update code to use the new apis to wait for service

--- examples/connext_dds/remote_procedure_call/py/Inventory.py
+++ examples/connext_dds/remote_procedure_call/py/Inventory.py
@@ -1,46 +1,33 @@
+#
+# (c) 2023 Copyright, Real-Time Innovations, Inc.  All rights reserved.
+#
+# RTI grants Licensee a license to use, modify, compile, and create derivative
+# works of the Software solely for use with RTI products.  The Software is
+# provided "as is", with no warranty of any type, including any warranty for
+# fitness for any purpose. RTI is under no obligation to maintain or support
+# the Software.  RTI shall not be liable for any incidental or consequential
+# damages arising out of the use or inability to use the software.
+#
 
-# WARNING: THIS FILE IS AUTO-GENERATED. DO NOT MODIFY.
-
-# This file was generated from Inventory.idl
-# using RTI Code Generator (rtiddsgen) version 4.4.0.
-# The rtiddsgen tool is part of the RTI Connext DDS distribution.
-# For more information, type 'rtiddsgen -help' at a command shell
-# or consult the Code Generator User's Manual.
-
+from abc import ABC
 from dataclasses import field
-from typing import Union, Sequence, Optional
+from typing import Sequence
 import rti.idl as idl
 import rti.rpc as rpc
-from enum import IntEnum
-import sys
-import os
-from abc import ABC
 
 
-
-@idl.struct(
-    member_annotations = {
-        'name': [idl.bound(255)],
-    }
-)
+@idl.struct
 class Item:
     name: str = ""
     quantity: int = 0
 
-@idl.struct(
-    member_annotations = {
-        'items': [idl.bound(100)],
-    }
-)
+
+@idl.struct
 class InventoryContents:
-    items: Sequence[Item] = field(default_factory = list)
+    items: Sequence[Item] = field(default_factory=list)
 
 
-@idl.struct(
-    member_annotations = {
-        'name': [idl.bound(255)],
-    }
-)
+@idl.struct
 class UnknownItemError(Exception):
     name: str = ""
 
@@ -48,16 +35,16 @@ class UnknownItemError(Exception):
 @rpc.service
 class InventoryService(ABC):
     @rpc.operation
-    async def get_inventory(self) -> InventoryContents:
+    def get_inventory(self) -> InventoryContents:
         """Get the current inventory inventory"""
         ...
 
     @rpc.operation
-    async def add_item(self, item: Item):
+    def add_item(self, item: Item):
         """Add quantity to an existing item"""
         ...
 
     @rpc.operation(raises=[UnknownItemError])
-    async def remove_item(self, item: Item):
+    def remove_item(self, item: Item):
         """Remove quantity from an existing item"""
         ...
