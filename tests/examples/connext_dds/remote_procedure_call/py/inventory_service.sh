ab692232275cd3e203bc2ffb61eadcdbab3320b4
Alberto Robles <alberto.robles.hernandez@gmail.com>
2024-11-06 15:20:15 +0100

    REQREPLY-221: Fix testing

--- tests/examples/connext_dds/remote_procedure_call/py/inventory_service.sh
+++ tests/examples/connext_dds/remote_procedure_call/py/inventory_service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /test/connextdds_examples_suite/connextdds_examples_repo/examples/connext_dds/remote_procedure_call/py
+cd /test/connextdds_examples_suite/rticonnextdds-examples/examples/connext_dds/remote_procedure_call/py
 /bin/python3 inventory_service.py $@
 rm -rf __pycache__
--- tests/examples/connext_dds/request_reply/py/primes_requester.sh
+++ tests/examples/connext_dds/request_reply/py/primes_requester.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /test/connextdds_examples_suite/connextdds_examples_repo/examples/connext_dds/request_reply/py
+cd /test/connextdds_examples_suite/rticonnextdds-examples/examples/connext_dds/request_reply/py
 /bin/python3 primes_requester.py $@
 rm -rf __pycache__
