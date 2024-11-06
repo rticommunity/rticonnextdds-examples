55f23c3f2ceb26141147ff37a73852511a864322
Alberto Robles <alberto.robles.hernandez@gmail.com>
2024-11-06 10:40:10 +0100

    REQREPLY-221: Undo change in script

--- tests/examples/connext_dds/remote_procedure_call/py/inventory_client.sh
+++ tests/examples/connext_dds/remote_procedure_call/py/inventory_client.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "$(dirname "$0")"
-SCRIPT_DIR="$(pwd)"
-echo "Script directory: $SCRIPT_DIR"
-
-
-# cd /test/connextdds_examples_suite/connextdds_examples_repo/examples/connext_dds/remote_procedure_call/py
-# /bin/python3 inventory_client.py $@
-# rm -rf __pycache__
+cd /test/connextdds_examples_suite/connextdds_examples_repo/examples/connext_dds/remote_procedure_call/py
+/bin/python3 inventory_client.py $@
+rm -rf __pycache__
